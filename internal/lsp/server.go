e0f53c653290459a0339452cf50886def8e1ce5b
Matthias Simon <matthias.simon@nokia.com>
2020-02-10 10:58:44 +0100

    Add server

--- internal/lsp/server.go
+++ internal/lsp/server.go
@@ -1,13 +1,28 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package lsp implements LSP for gopls.
 package lsp
 
 import (
 	"context"
-	"os"
 	"sync"
 
 	"github.com/nokia/ntt/internal/jsonrpc2"
 	"github.com/nokia/ntt/internal/lsp/protocol"
+	"github.com/nokia/ntt/internal/span"
 )
+
+// NewServer creates an LSP server and binds it to handle incoming client
+// messages on on the supplied stream.
+func NewServer(session source.Session, client protocol.Client) *Server {
+	return &Server{
+		delivered: make(map[span.URI]sentDiagnostics),
+		session:   session,
+		client:    client,
+	}
+}
 
 type serverState int
 
@@ -18,259 +33,59 @@ const (
 	serverShutDown
 )
 
+// Server implements the protocol.Server interface.
 type Server struct {
-	Conn   *jsonrpc2.Conn
 	client protocol.Client
 
 	stateMu sync.Mutex
 	state   serverState
 
-	rootURI string
+	// folders is only valid between initialize and initialized, and holds the
+	// set of folders to build views for when we are ready
+	pendingFolders []protocol.WorkspaceFolder
 }
 
-func NewServer(ctx context.Context, stream jsonrpc2.Stream) (context.Context, *Server) {
-	s := &Server{}
-	ctx, s.Conn, s.client = protocol.NewServer(ctx, stream, s)
-	return ctx, s
+func (s *Server) cancelRequest(ctx context.Context, params *protocol.CancelParams) error {
+	return nil
 }
 
-func (s *Server) Run(ctx context.Context) error {
-	return s.Conn.Run(ctx)
+func (s *Server) codeLens(ctx context.Context, params *protocol.CodeLensParams) ([]protocol.CodeLens, error) {
+	return nil, nil
 }
 
-func (s *Server) Info(ctx context.Context, msg string) {
-	s.client.ShowMessage(ctx, &protocol.ShowMessageParams{
-		Type:    protocol.Info,
-		Message: msg,
-	})
-}
-
-func (s *Server) Log(ctx context.Context, msg string) {
-	s.client.LogMessage(ctx, &protocol.LogMessageParams{
-		Type:    protocol.Log,
-		Message: msg,
-	})
-}
-
-// General
-
-func (s *Server) Initialize(ctx context.Context, params *protocol.ParamInitia) (*protocol.InitializeResult, error) {
-	s.stateMu.Lock()
-	state := s.state
-	s.stateMu.Unlock()
-	if state >= serverInitializing {
-		return nil, jsonrpc2.NewErrorf(jsonrpc2.CodeInvalidRequest, "server already initialized")
+func (s *Server) nonstandardRequest(ctx context.Context, method string, params interface{}) (interface{}, error) {
+	paramMap := params.(map[string]interface{})
+	if method == "gopls/diagnoseFiles" {
+		for _, file := range paramMap["files"].([]interface{}) {
+			uri := span.NewURI(file.(string))
+			view, err := s.session.ViewOf(uri)
+			if err != nil {
+				return nil, err
+			}
+			fileID, diagnostics, err := source.FileDiagnostics(ctx, view.Snapshot(), uri)
+			if err != nil {
+				return nil, err
+			}
+			if err := s.client.PublishDiagnostics(ctx, &protocol.PublishDiagnosticsParams{
+				URI:         protocol.NewURI(uri),
+				Diagnostics: toProtocolDiagnostics(diagnostics),
+				Version:     fileID.Version,
+			}); err != nil {
+				return nil, err
+			}
+		}
+		if err := s.client.PublishDiagnostics(ctx, &protocol.PublishDiagnosticsParams{
+			URI: "gopls://diagnostics-done",
+		}); err != nil {
+			return nil, err
+		}
+		return struct{}{}, nil
 	}
-	s.stateMu.Lock()
-	s.state = serverInitializing
-	s.stateMu.Unlock()
-
-	s.rootURI = params.RootURI
-
-	return &protocol.InitializeResult{
-		Capabilities: protocol.ServerCapabilities{
-			DefinitionProvider: true,
-			TextDocumentSync: &protocol.TextDocumentSyncOptions{
-				Change:    protocol.Incremental,
-				OpenClose: true,
-				Save: &protocol.SaveOptions{
-					IncludeText: false,
-				},
-			},
-			Workspace: &struct {
-				WorkspaceFolders *struct {
-					Supported           bool   "json:\"supported,omitempty\""
-					ChangeNotifications string "json:\"changeNotifications,omitempty\""
-				} "json:\"workspaceFolders,omitempty\""
-			}{
-				WorkspaceFolders: &struct {
-					Supported           bool   "json:\"supported,omitempty\""
-					ChangeNotifications string "json:\"changeNotifications,omitempty\""
-				}{
-					Supported: false,
-				},
-			},
-		},
-	}, nil
-
-}
-
-func (s *Server) Initialized(ctx context.Context, params *protocol.InitializedParams) error {
-	s.stateMu.Lock()
-	s.state = serverInitialized
-	s.stateMu.Unlock()
-
-	s.Log(ctx, "K3 language server initialized")
-	return nil
-}
-
-func (s *Server) Shutdown(ctx context.Context) error {
-	s.stateMu.Lock()
-	defer s.stateMu.Unlock()
-	if s.state < serverInitialized {
-		return jsonrpc2.NewErrorf(jsonrpc2.CodeInvalidRequest, "server not initialized")
-	}
-	// drop all the active views
-	s.state = serverShutDown
-	return nil
-}
-
-func (s *Server) Exit(ctx context.Context) error {
-	s.stateMu.Lock()
-	defer s.stateMu.Unlock()
-	if s.state != serverShutDown {
-		os.Exit(1)
-	}
-	os.Exit(0)
-	return nil
-}
-
-func (s *Server) ChancelRequest(ctx context.Context, params *protocol.CancelParams) error {
-	return nil
-}
-
-// Workspace
-
-func (s *Server) DidChangeWorkspaceFolders(ctx context.Context, params *protocol.DidChangeWorkspaceFoldersParams) error {
-	return notImplemented("DidChangeWorkspaceFolders")
-}
-
-func (s *Server) DidChangeConfiguration(ctx context.Context, params *protocol.DidChangeConfigurationParams) error {
-	return notImplemented("DidChangeConfiguration")
-}
-
-func (s *Server) DidChangeWatchedFiles(ctx context.Context, params *protocol.DidChangeWatchedFilesParams) error {
-	return notImplemented("DidChangeWatchedFiles")
-}
-
-func (s *Server) Symbol(ctx context.Context, params *protocol.WorkspaceSymbolParams) ([]protocol.SymbolInformation, error) {
-	return nil, notImplemented("Symbol")
-}
-
-func (s *Server) ExecuteCommand(ctx context.Context, params *protocol.ExecuteCommandParams) (interface{}, error) {
-	return nil, notImplemented("ExecuteCommand")
-}
-
-// Text Synchronization
-
-func (s *Server) DidOpen(ctx context.Context, params *protocol.DidOpenTextDocumentParams) error {
-	return nil
-}
-
-func (s *Server) DidChange(ctx context.Context, params *protocol.DidChangeTextDocumentParams) error {
-	return nil
-}
-
-func (s *Server) WillSave(ctx context.Context, params *protocol.WillSaveTextDocumentParams) error {
-	return notImplemented("WillSave")
-}
-
-func (s *Server) WillSaveWaitUntil(ctx context.Context, params *protocol.WillSaveTextDocumentParams) ([]protocol.TextEdit, error) {
-	return nil, notImplemented("WillSaveWaitUntil")
-}
-
-func (s *Server) DidSave(ctx context.Context, params *protocol.DidSaveTextDocumentParams) error {
-	return nil
-}
-
-func (s *Server) DidClose(ctx context.Context, params *protocol.DidCloseTextDocumentParams) error {
-	return nil
-}
-
-// Language Features
-
-func (s *Server) Completion(ctx context.Context, params *protocol.CompletionParams) (*protocol.CompletionList, error) {
-	return nil, notImplemented("Completion")
-}
-
-func (s *Server) Resolve(ctx context.Context, params *protocol.CompletionItem) (*protocol.CompletionItem, error) {
-	return nil, notImplemented("Resolve")
-}
-
-func (s *Server) Hover(ctx context.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
-	return nil, notImplemented("Hover")
-}
-
-func (s *Server) SignatureHelp(ctx context.Context, params *protocol.SignatureHelpParams) (*protocol.SignatureHelp, error) {
-	return nil, notImplemented("SignatureHelp")
-}
-
-func (s *Server) Definition(ctx context.Context, params *protocol.DefinitionParams) ([]protocol.Location, error) {
-	return nil, notImplemented("Definition")
-}
-func (s *Server) TypeDefinition(ctx context.Context, params *protocol.TypeDefinitionParams) ([]protocol.Location, error) {
-	return nil, notImplemented("TypeDefinition")
-}
-func (s *Server) Implementation(ctx context.Context, params *protocol.ImplementationParams) ([]protocol.Location, error) {
-	return nil, notImplemented("Implementation")
-}
-func (s *Server) References(ctx context.Context, params *protocol.ReferenceParams) ([]protocol.Location, error) {
-	return nil, notImplemented("References")
-}
-func (s *Server) DocumentHighlight(ctx context.Context, params *protocol.DocumentHighlightParams) ([]protocol.DocumentHighlight, error) {
-	return nil, notImplemented("DocumentHighlight")
-}
-func (s *Server) DocumentSymbol(ctx context.Context, params *protocol.DocumentSymbolParams) ([]protocol.DocumentSymbol, error) {
-	return nil, notImplemented("DocumentSymbol")
-}
-func (s *Server) CodeAction(ctx context.Context, params *protocol.CodeActionParams) ([]protocol.CodeAction, error) {
-	return nil, notImplemented("CodeAction")
-}
-
-func (s *Server) CodeLens(ctx context.Context, params *protocol.CodeLensParams) ([]protocol.CodeLens, error) {
-	return nil, notImplemented("CodeLens")
-}
-func (s *Server) ResolveCodeLens(ctx context.Context, params *protocol.CodeLens) (*protocol.CodeLens, error) {
-	return nil, notImplemented("ResolveCodeLens")
-}
-func (s *Server) DocumentLink(ctx context.Context, params *protocol.DocumentLinkParams) ([]protocol.DocumentLink, error) {
-	return nil, notImplemented("DocumentLink")
-}
-func (s *Server) ResolveDocumentLink(ctx context.Context, params *protocol.DocumentLink) (*protocol.DocumentLink, error) {
-	return nil, notImplemented("ResolveDocumentLink")
-}
-func (s *Server) DocumentColor(ctx context.Context, params *protocol.DocumentColorParams) ([]protocol.ColorInformation, error) {
-	return nil, notImplemented("DocumentColor")
-}
-func (s *Server) ColorPresentation(ctx context.Context, params *protocol.ColorPresentationParams) ([]protocol.ColorPresentation, error) {
-	return nil, notImplemented("ColorPresentation")
-}
-func (s *Server) Formatting(ctx context.Context, params *protocol.DocumentFormattingParams) ([]protocol.TextEdit, error) {
-	return nil, notImplemented("Formatting")
-}
-
-func (s *Server) RangeFormatting(ctx context.Context, params *protocol.DocumentRangeFormattingParams) ([]protocol.TextEdit, error) {
-	return nil, notImplemented("RangeFormatting")
-}
-
-func (s *Server) OnTypeFormatting(ctx context.Context, params *protocol.DocumentOnTypeFormattingParams) ([]protocol.TextEdit, error) {
-	return nil, notImplemented("OnTypeFormatting")
-}
-func (s *Server) Rename(ctx context.Context, params *protocol.RenameParams) (*protocol.WorkspaceEdit, error) {
-	return nil, notImplemented("Rename")
-}
-func (s *Server) Declaration(ctx context.Context, params *protocol.DeclarationParams) ([]protocol.DeclarationLink, error) {
-	return nil, notImplemented("Declaration")
-}
-func (s *Server) FoldingRange(ctx context.Context, params *protocol.FoldingRangeParams) ([]protocol.FoldingRange, error) {
-	return nil, notImplemented("FoldingRange")
-}
-func (s *Server) LogTraceNotification(ctx context.Context, params *protocol.LogTraceParams) error {
-	return notImplemented("LogTraceNotification")
-}
-func (s *Server) PrepareRename(ctx context.Context, params *protocol.PrepareRenameParams) (*protocol.Range, error) {
-	return nil, notImplemented("PrepareRename")
-}
-func (s *Server) Progress(ctx context.Context, params *protocol.ProgressParams) error {
-	return notImplemented("Progress")
-}
-func (s *Server) SetTraceNotification(ctx context.Context, params *protocol.SetTraceParams) error {
-	return nil
-}
-func (s *Server) SelectionRange(ctx context.Context, params *protocol.SelectionRangeParams) ([]protocol.SelectionRange, error) {
-	return nil, notImplemented("SelectionRange")
+	return nil, notImplemented(method)
 }
 
 func notImplemented(method string) *jsonrpc2.Error {
 	return jsonrpc2.NewErrorf(jsonrpc2.CodeMethodNotFound, "method %q not yet implemented", method)
 }
+
+//go:generate helper/helper -d protocol/tsserver.go -o server_gen.go -u .
