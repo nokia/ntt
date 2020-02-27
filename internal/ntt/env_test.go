a1b9929125e59a686322df523364f9f36b5694a3
Matthias Simon <matthias.simon@nokia.com>
2020-02-27 11:34:03 +0100

    Add tests for Getenv

--- internal/ntt/env_test.go
+++ internal/ntt/env_test.go
@@ -1,12 +1,37 @@
 package ntt_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/nokia/ntt/internal/ntt"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestEnv(t *testing.T) {
-	//suite := &ntt.Suite{}
-	//s, err := suite.Getenv("name")
-	//assert.Nil(t, err)
-	//assert.Equal(t, "", s)
+func TestEnvEmpty(t *testing.T) {
+	defer os.Unsetenv("NTT_FNORD")
+
+	suite := &ntt.Suite{}
+	s := suite.Getenv("NTT_FNORD")
+	assert.Equal(t, "", s)
+}
+
+func TestEnvSimple(t *testing.T) {
+	defer os.Unsetenv("NTT_FNORD")
+	os.Setenv("NTT_FNORD", "23.5")
+
+	suite := &ntt.Suite{}
+	s := suite.Getenv("NTT_FNORD")
+	assert.Equal(t, "23.5", s)
+}
+
+func TestEnvK3(t *testing.T) {
+	os.Unsetenv("NTTFNORD")
+
+	defer os.Unsetenv("K3FNORD")
+	os.Setenv("K3FNORD", "23.5")
+
+	suite := &ntt.Suite{}
+	s := suite.Getenv("NTTFNORD")
+	assert.Equal(t, "23.5", s)
 }
