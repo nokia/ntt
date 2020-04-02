001623879e02d039499ba568fe79787008008c4d
Matthias Simon <matthias.simon@nokia.com>
2020-04-02 18:01:05 +0200

    Restored tests

--- internal/ntt/symbols_test.go
+++ internal/ntt/symbols_test.go
@@ -1,32 +1,23 @@
 package ntt_test
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/nokia/ntt/internal/ntt"
+	"github.com/stretchr/testify/assert"
+	"github.com/y0ssar1an/q"
 )
 
-//func buildSuite(t *testing.T, strs ...string) *ntt.Suite {
-//	suite := &ntt.Suite{}
-//	for i, s := range strs {
-//		name := fmt.Sprintf("%s_Module_%d.ttcn3", t.Name(), i)
-//		file := suite.File(name)
-//		file.SetBytes([]byte(s))
-//	}
-//	return suite
-//}
-//
-//func symbols(t *testing.T, strs ...string) (*ntt.Suite, []*ntt.Module) {
-//	suite := buildSuite(t, strs...)
-//
-//	mods := make([]*ntt.Module, len(strs))
-//	errs := make([]error, len(strs))
-//
-//	for i, s := range strs {
-//		name := fmt.Sprintf("%s_Module_%d.ttcn3", t.Name(), i)
-//		mods[i] = suite.symbols(name)
-//	}
-//
-//	return suite, mods
-//}
+func buildSuite(t *testing.T, strs ...string) *ntt.Suite {
+	suite := &ntt.Suite{}
+	for i, s := range strs {
+		name := fmt.Sprintf("%s_Module_%d.ttcn3", t.Name(), i)
+		file := suite.File(name)
+		file.SetBytes([]byte(s))
+	}
+	return suite
+}
 
 // Import handling.
 // TODO: func TestImport(t *testing.T) {}
@@ -40,29 +31,46 @@ import (
 // TODO: func TestEnumType(t *testing.T)      {}
 // TODO: func TestBehaviourType(t *testing.T) {}
 func TestStructType(t *testing.T) {
-	//	_, mods, err := symbols(t, `module Test
-	//	{
-	//	    control {
-	//			var Rec r
-	//			r.x := 23
-	//			r.next.next.x := 5
-	//			r.x := r.y.x
-	//			r.a := 4         // ERR: No such field a.
-	//	    }
-	//
-	//	    type record Rec {
-	//			integer x,
-	//			Rec     next      // ERR: Recursive field is non-optional
-	//			struct {
-	//				int x
-	//			} y
-	//		}
-	//
-	//    }`)
-	//
-	//	m := mods[0]
-	//	assert.NotNil(t, m)
-	//	q.Q(err)
+	suite := buildSuite(t, `module Test
+	{
+	    control {
+			var Rec r
+			r.x := 23
+			r.next.next.x := 5
+			r.x := r.y.x
+			r.a := 4         // ERR: No such field a.
+	    }
+	
+	    type record Rec {
+			integer x,
+			Rec     next      // ERR: Recursive field is non-optional
+			struct {
+				int x
+			} y
+		}
+	
+	  }`)
+
+	// Lookup `Rec`
+	id, _ := suite.IdentifierAt("TestStructType_Module_0.ttcn3", 4, 8)
+	assert.Equal(t, 11, id.Line(id.Def.Pos()))
+	assert.Equal(t, 18, id.Column(id.Def.Pos()))
+
+	// Lookup `r` (LHS)
+	id, _ = suite.IdentifierAt("TestStructType_Module_0.ttcn3", 7, 11)
+	assert.Equal(t, 4, id.Line(id.Def.Pos()))
+	assert.Equal(t, 12, id.Column(id.Def.Pos()))
+
+	// Lookup `r` (RHS)
+	id, _ = suite.IdentifierAt("TestStructType_Module_0.ttcn3", 8, 4)
+	assert.Equal(t, 4, id.Line(id.Def.Pos()))
+	assert.Equal(t, 12, id.Column(id.Def.Pos()))
+
+	// Lookup `r.x` (RHS)
+	id, _ = suite.IdentifierAt("TestStructType_Module_0.ttcn3", 8, 6)
+	q.Q(id)
+	assert.Equal(t, 12, id.Line(id.Def.Pos()))
+	assert.Equal(t, 12, id.Column(id.Def.Pos()))
 }
 
 // Other
@@ -73,26 +81,27 @@ func TestStructType(t *testing.T) {
 // TODO: func TestFunc(t *testing.T)      {}
 
 func TestDecl(t *testing.T) {
-	//	suite, mods, _ := symbols(t, `module Test
-	//	{
-	//	    control {
-	//			var boolean x;         // ERR: Shadowing is not permitted.
-	//			const integer a := b;  // ERR: In local scopes, b must be declared before a.
-	//			const integer b := 23;
-	//			const integer b := 5;  // ERR: Redeclaration of b.
-	//	    }
-	//
-	//	    const integer x := y;
-	//	    const integer y := 23;
-	//
-	//    }`)
-	//
-	//	m := mods[0]
-	//	assert.NotNil(t, m)
-	//
-	//	assert.Equal(t, []string{"x", "y"}, m.Names())
-	//	v := m.Lookup("x")
-	//	assert.Equal(t, "x", v.Name())
-	//	assert.Equal(t, 10, suite.Position("TestDecl_Module_0.ttcn3", v.Pos()).Line)
-	//	assert.Equal(t, m, v.Parent())
+	suite := buildSuite(t, `module Test
+	{
+	    control {
+			var boolean x;         // ERR: Shadowing is not permitted.
+			const integer a := b;  // ERR: In local scopes, b must be declared before a.
+			const integer b := y;
+			const integer b := 5;  // ERR: Redeclaration of b.
+	    }
+	
+	    const integer x := y;
+	    const integer y := 23;
+	
+	}`)
+
+	// Lookup `b`
+	id, _ := suite.IdentifierAt("TestDecl_Module_0.ttcn3", 5, 23)
+	assert.Equal(t, 6, id.Line(id.Def.Pos()))
+	assert.Equal(t, 18, id.Column(id.Def.Pos()))
+
+	// Lookup `y`
+	id, _ = suite.IdentifierAt("TestDecl_Module_0.ttcn3", 6, 23)
+	assert.Equal(t, 11, id.Line(id.Def.Pos()))
+	assert.Equal(t, 20, id.Column(id.Def.Pos()))
 }
