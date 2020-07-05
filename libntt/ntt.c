a55bb9ae452878c8541999b98aca319dda88183c
Peter Franzreb <pefra23@gmail.com>
2020-07-05 20:33:52 +0200

    Add c dummy functions

---
+++ libntt/ntt.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+#include "ntt.h"
+
+char * ntt_list_testcases(const char * path) {
+	assert(path != NULL);
+
+	return NULL;
+}
+char * ntt_list_imports(const char * path) {
+	assert(path != NULL);
+
+	return NULL;
+}
+
+
---
+++ libntt/ntt.h
@@ -0,0 +1,7 @@
+#ifndef NTT_H
+#define NTT_H
+
+char * ntt_list_testcases(const char * path);
+char * ntt_list_imports(const char * path);
+
+#endif
