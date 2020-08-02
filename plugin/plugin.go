c7a2021c8375839762dd14ed64264f8a842b0fc7
Matthias Simon <matthias.simon@nokia.com>
2020-08-02 11:18:51 +0200

    Refactor Go Proto

--- plugin/plugin.go
+++
@@ -1,2 +0,0 @@
-//go:generate protoc  -I. -I..  --go_out=. --go_opt=paths=source_relative plugin.proto
-package plugin
--- ttcn3/ttcn3.go
+++
@@ -1,2 +0,0 @@
-//go:generate protoc --go_out=. --go_opt=paths=source_relative ttcn3.proto
-package ttcn3
