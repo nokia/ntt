6a2d12576d3f0fba928315154d03ff4c14fa23fb
Matthias Simon <matthias.simon@nokia.com>
2021-02-17 10:34:59 +0100

    Extend show command with sh-setup feature

--- cmd/ntt/main.go
+++ cmd/ntt/main.go
@@ -68,6 +68,7 @@ var (
 	}
 
 	Verbose = false
+	ShSetup = false
 
 	version = "dev"
 	commit  = "none"
@@ -79,6 +80,7 @@ var (
 func init() {
 	session.SharedDir = "/tmp/k3"
 	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVarP(&ShSetup, "sh-setup", "", false, "output test suite data for shell consumption")
 	rootCmd.PersistentFlags().StringVarP(&cpuprofile, "cpuprofile", "", "", "write cpu profile to `file`")
 	rootCmd.AddCommand(showCmd)
 	rootCmd.AddCommand(dump.Command)
--- cmd/ntt/show.go
+++ cmd/ntt/show.go
@@ -120,26 +120,89 @@ func show(cmd *cobra.Command, args []string) error {
 		os.Exit(1)
 	}
 
-	if len(keys) == 0 {
-		for _, key := range []string{
-			"name",
-			"sources",
-			"imports",
-			"timeout",
-			"parameters_file",
-			"test_hook",
-			"source_dir",
-			"datadir",
-			"session_id",
-		} {
-			if s := stringers[key](suite); s != "" {
-				fmt.Printf("K3_%s=\"%s\"\n", strings.ToUpper(key), strings.Replace(s, "\n", " ", -1))
-			}
-		}
+	switch {
+	case ShSetup:
+		return printShellScript(suite, keys)
+	case len(keys) != 0:
+		return printUserKeys(suite, keys)
+	default:
+		return printDefaultKeys(suite)
+	}
+}
 
-		return nil
+func printShellScript(suite *ntt.Suite, keys []string) error {
+	if len(keys) != 0 {
+		return fmt.Errorf("command line option --sh-setup does not accept additional command line arguments")
 	}
 
+	for _, key := range []string{
+		"name",
+		"timeout",
+		"parameters_file",
+		"test_hook",
+		"source_dir",
+		"datadir",
+		"session_id",
+	} {
+		if s := stringers[key](suite); s != "" {
+			fmt.Printf("K3_%s=\"%s\"\n", strings.ToUpper(key), strings.Replace(s, "\n", " ", -1))
+		}
+	}
+
+	fmt.Print(`
+# k3-cached-file searches for file $1 in $K3_CACHE and $NTT_CACHE and returns its first
+# occurence.
+function k3-cached-file()
+{
+    local IFS=:
+    read -r -a dirs <<<"$K3_CACHE:$NTT_CACHE"
+    for dir in "${dirs[@]}"; do
+        local cached_file="$dir/$1"
+        if [ -e "$cached_file" ]; then
+            echo "$cached_file"
+            return
+        fi
+    done
+    echo "$1"
+}
+
+`)
+
+	env, err := suite.Environ()
+	if err != nil {
+		fatal(err)
+	}
+	sort.Strings(env)
+	for _, e := range env {
+		fmt.Printf("export %s\n", e)
+	}
+	fmt.Println()
+
+	fmt.Println("K3_SOURCES=(")
+	srcs, err := suite.Sources()
+	for _, src := range ntt.PathSlice(srcs...) {
+		fmt.Printf("\t%s\n", src)
+	}
+	fmt.Print(")\n\n")
+
+	fmt.Println("K3_IMPORTS=(")
+	imps, err := suite.Imports()
+	for _, imp := range ntt.PathSlice(imps...) {
+		fmt.Printf("\t%s\n", imp)
+	}
+	fmt.Print(")\n\n")
+
+	fmt.Println("K3_TTCN3_FILES=(")
+	files, err := suite.Files()
+	for _, f := range append(files, ntt.FindAuxiliaryTTCN3Files()...) {
+		fmt.Printf("\t%s\n", f)
+	}
+	fmt.Print(")\n\n")
+
+	return nil
+}
+
+func printUserKeys(suite *ntt.Suite, keys []string) error {
 	for _, key := range keys {
 		if fun, found := stringers[key]; found {
 			if s := fun(suite); s != "" {
@@ -155,6 +218,26 @@ func show(cmd *cobra.Command, args []string) error {
 
 		fmt.Println(s)
 	}
+	return nil
+}
+
+func printDefaultKeys(suite *ntt.Suite) error {
+	for _, key := range []string{
+		"name",
+		"sources",
+		"imports",
+		"timeout",
+		"parameters_file",
+		"test_hook",
+		"source_dir",
+		"datadir",
+		"session_id",
+	} {
+		if s := stringers[key](suite); s != "" {
+			fmt.Printf("K3_%s=\"%s\"\n", strings.ToUpper(key), strings.Replace(s, "\n", " ", -1))
+		}
+	}
+
 	return nil
 }
 
