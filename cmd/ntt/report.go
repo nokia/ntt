be6d601d2613c61648e4067b3d174d56e6f9bb07
Matthias Simon <5nord@users.noreply.github.com>
2021-02-18 22:27:39 +0100

    Add k3 specific variables to k3-sh-setup (#167)

--- cmd/ntt/report.go
+++ cmd/ntt/report.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/nokia/ntt/internal/ntt"
 )
@@ -21,6 +25,12 @@ type Report struct {
 	Imports        []string `json:"imports"`
 	Files          []string `json:"files"`
 	AuxFiles       []string `json:"aux_files"`
+	OssInfo        string   `json:"ossinfo"`
+	K3             struct {
+		Compiler string   `json:"compiler"`
+		Runtime  string   `json:"runtime"`
+		Builtins []string `json:"builtins"`
+	} `json:"k3"`
 
 	suite *ntt.Suite
 }
@@ -75,11 +85,114 @@ func NewReport(args []string) *Report {
 
 	if root := r.suite.Root(); root != nil {
 		r.SourceDir = root.Path()
+		if path, err := filepath.Abs(r.SourceDir); err == nil {
+			r.SourceDir = path
+		}
 	}
 
 	r.AuxFiles = ntt.FindAuxiliaryTTCN3Files()
 
+	r.K3.Compiler = findK3Tool(r.suite, "mtc", "k3c")
+	r.K3.Runtime = findK3Tool(r.suite, "k3r")
+
+	r.OssInfo, _ = r.suite.Getenv("OSSINFO")
+	hint := filepath.Dir(r.K3.Runtime)
+	switch {
+	// Probably a regular K3 installation. We assume datadir and libdir are
+	// in a sibling folder.
+	case strings.HasSuffix(hint, "/bin"):
+		r.K3.Builtins = collectFolders(
+			hint+"/../lib*/k3/plugins",
+			hint+"/../lib*/k3/plugins/ttcn3",
+			hint+"/../lib/*/k3/plugins",
+			hint+"/../lib/*/k3/plugins/ttcn3",
+			hint+"/../share/k3/ttcn3",
+		)
+		if r.OssInfo == "" {
+			r.OssInfo = filepath.Clean(hint + "/../share/asn1")
+		}
+
+	// If the runtime seems to be a buildtree of our source repository, we
+	// assume the builtins are there as well.
+	case strings.HasSuffix(hint, "/src/k3r"):
+		// TODO(5nord) the last glob fails if CMAKE_BUILD_DIR is not
+		// beneath CMAKE_SOURCE_DIR. Find a way to locate the source
+		// dir correctly.
+		srcdir := hint + "/../../.."
+
+		r.K3.Builtins = collectFolders(
+			hint+"/../k3r-*-plugin",
+			srcdir+"/src/k3r-*-plugin",
+			srcdir+"/src/ttcn3",
+			srcdir+"/src/libzmq",
+		)
+	}
+
 	return &r
+}
+
+func findK3Tool(suite *ntt.Suite, names ...string) string {
+	for _, name := range names {
+		if env, _ := suite.Getenv(strings.ToUpper(name)); env != "" {
+			name = env
+		}
+		if path, err := exec.LookPath(name); err == nil {
+			return path
+		}
+	}
+	return ""
+}
+
+func collectFolders(globs ...string) []string {
+	return removeDuplicates(filterFolders(evalSymlinks(resolveGlobs(globs))))
+}
+
+func resolveGlobs(globs []string) []string {
+	var ret []string
+
+	for _, g := range globs {
+		if matches, err := filepath.Glob(g); err == nil {
+			ret = append(ret, matches...)
+		}
+	}
+	return ret
+}
+
+func evalSymlinks(links []string) []string {
+	var ret []string
+	for _, l := range links {
+		if path, err := filepath.EvalSymlinks(l); err == nil {
+			ret = append(ret, path)
+		}
+	}
+	return ret
+}
+
+func filterFolders(paths []string) []string {
+	var ret []string
+	for _, path := range paths {
+		info, err := os.Stat(path)
+		if err != nil {
+			continue
+		}
+
+		if info.IsDir() {
+			ret = append(ret, path)
+		}
+	}
+	return ret
+}
+
+func removeDuplicates(slice []string) []string {
+	var ret []string
+	h := make(map[string]bool)
+	for _, s := range slice {
+		if _, v := h[s]; !v {
+			h[s] = true
+			ret = append(ret, s)
+		}
+	}
+	return ret
 }
 
 func path(f *ntt.File, err error) (string, error) {
