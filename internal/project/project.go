14ca64a25f9bf27770354b24016b859a98cccb17
Matthias Simon <matthias.simon@nokia.com>
2021-07-27 09:05:29 +0200

    Rename suites package to project

--- internal/project/project.go
+++ internal/project/project.go
@@ -1,6 +1,11 @@
-// Package suites help finding TTCN-3 suites by implementing various heuristics
-// about test suite organization.
-package suites
+// Package project collects information about test suite organisation by
+// implementing various heuristics.
+//
+// A language server, for example, must always know which TTCN-3 module belongs
+// to what test suite. However, there's no standard way of telling where to
+// actually look for. Not always do have a manifest file available. Therefore
+// we must rely on various heuristics and provide a best effort solution.
+package project
 
 // Build struct provides information about build directory organisation.
 // Usually the build system write this information into the file
