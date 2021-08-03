40b8189416b77a193b8524851bed569f89128088
Matthias Simon <matthias.simon@nokia.com>
2021-08-03 14:27:05 +0200

    Move tests

--- internal/fs/file_test.go
+++ internal/fs/file_test.go
@@ -1,4 +1,4 @@
-package ntt_test
+package fs_test
 
 import (
 	"testing"
@@ -12,20 +12,20 @@ import (
 //
 // This test checks if content of files and file names are handled correctly.
 func TestFiles(t *testing.T) {
-	f1 := fs.Open("../ntt/ntt_test.go")
-	f2 := fs.Open("../../internal/ntt/ntt_test.go")
+	f1 := fs.Open("../fs/files_test.go")
+	f2 := fs.Open("../../internal/fs/files_test.go")
 	assert.NotNil(t, f1)
 	assert.NotNil(t, f2)
 	assert.Equal(t, f1, f2)
 
-	f1 = fs.Open("../ntt/does_not_exist")
-	f2 = fs.Open("../../internal/ntt/does_not_exist")
+	f1 = fs.Open("../fs/does_not_exist")
+	f2 = fs.Open("../../internal/fs/does_not_exist")
 	assert.NotNil(t, f1)
 	assert.NotNil(t, f2)
 	assert.Equal(t, f1, f2)
 
-	f1 = fs.Open("../ntt/ntt_test.go")
-	f2 = fs.Open("../ntt/does_not_exist")
+	f1 = fs.Open("../fs/files_test.go")
+	f2 = fs.Open("../fs/does_not_exist")
 	assert.NotNil(t, f1)
 	assert.NotNil(t, f2)
 	assert.NotEqual(t, f1, f2)
@@ -33,18 +33,18 @@ func TestFiles(t *testing.T) {
 
 func TestFileContent(t *testing.T) {
 	t.Run("DiskRead", func(t *testing.T) {
-		f1 := fs.Open("../ntt/ntt_test.go")
+		f1 := fs.Open("../fs/files_test.go")
 		b1, err := f1.Bytes()
 		assert.Nil(t, err)
 		if len(b1) >= 16 {
-			assert.Equal(t, []byte("package ntt_test"), b1[:16])
+			assert.Equal(t, []byte("package fs_test"), b1[:16])
 		} else {
 			t.Errorf("length of b1 < 16")
 		}
 	})
 
 	t.Run("DiskRead2", func(t *testing.T) {
-		f1 := fs.Open("../ntt/ntt_test.go")
+		f1 := fs.Open("../fs/files_test.go")
 		f1.SetBytes([]byte("fnord"))
 		b1, err := f1.Bytes()
 		assert.Nil(t, err)
@@ -52,7 +52,7 @@ func TestFileContent(t *testing.T) {
 	})
 
 	t.Run("DiskRead3", func(t *testing.T) {
-		f1 := fs.Open("../ntt/does_not_exist")
+		f1 := fs.Open("../fs/does_not_exist")
 		_, err := f1.Bytes()
 		assert.NotNil(t, err)
 		f1.SetBytes([]byte("fnord"))
@@ -62,13 +62,13 @@ func TestFileContent(t *testing.T) {
 	})
 
 	t.Run("DiskReadError", func(t *testing.T) {
-		f1 := fs.Open("../ntt")
+		f1 := fs.Open("../fs")
 		_, err := f1.Bytes()
 		assert.NotNil(t, err)
 	})
 
 	t.Run("DiskReadError2", func(t *testing.T) {
-		f1 := fs.Open("../ntt/does_not_exist")
+		f1 := fs.Open("../fs/does_not_exist")
 		f1.Reset()
 		_, err := f1.Bytes()
 		assert.NotNil(t, err)
