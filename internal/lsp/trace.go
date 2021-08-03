96d7b25cf53aca98e5197b7c2b035181c340ebb3
Matthias Simon <matthias.simon@nokia.com>
2021-08-03 14:11:14 +0200

    Let language server ignore loglevel

--- internal/lsp/trace.go
+++ internal/lsp/trace.go
@@ -3,6 +3,7 @@ package lsp
 import (
 	"context"
 
+	"github.com/nokia/ntt/internal/env"
 	"github.com/nokia/ntt/internal/log"
 	"github.com/nokia/ntt/internal/lsp/protocol"
 )
@@ -13,12 +14,17 @@ func (s *Server) setTrace(ctx context.Context, params *protocol.SetTraceParams) 
 }
 
 func setTrace(s string) {
+	// Ignore client settings when NTT_DEBUG is enabled
+	if env := env.Getenv("NTT_DEBUG"); env != "" {
+		log.SetGlobalLevel(log.DebugLevel)
+		return
+	}
+
 	switch s {
 	case "verbose":
 		log.SetGlobalLevel(log.DebugLevel)
 	case "off":
 		log.SetGlobalLevel(log.PrintLevel)
-	default:
 	}
 	return
 }
