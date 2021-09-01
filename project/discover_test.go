db4d6cc16f8f73ed8379cc3e389fb6e7e1511941
Matthias Simon <matthias.simon@nokia.com>
2021-09-01 10:14:24 +0200

    Fix tests

--- project/discover_test.go
+++ project/discover_test.go
@@ -9,13 +9,13 @@ import (
 
 func TestDiscoverManifests(t *testing.T) {
 	actual := project.Discover("testdata/manifests/suite1/testcases")
-	expected := []string{"testdata/manifests/suite1", "testdata/manifests/some-dir"}
+	expected := []string{"testdata/manifests/suite1", "testdata/manifests/obsolete", "testdata/manifests/some-dir"}
 	assert.Equal(t, expected, actual)
 
 }
 
 func TestDiscoverBuildDirectories(t *testing.T) {
 	actual := project.Discover("testdata/builds/suite1/testcases")
-	expected := []string{"testdata/builds/suite1/build", "testdata/builds/build/some-dir", "testdata/builds/suite2"}
+	expected := []string{"testdata/builds/suite1/build", "testdata/builds/build/obsolete", "testdata/builds/build/some-dir", "testdata/builds/suite2"}
 	assert.Equal(t, expected, actual)
 }
