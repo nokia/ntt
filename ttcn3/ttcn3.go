67c6ed116e72d942498a6d75438cf02eb1ed31e4
Matthias Simon <matthias.simon@nokia.com>
2021-09-08 10:09:33 +0200

    Add initial parser interface

---
+++ ttcn3/ttcn3.go
@@ -0,0 +1,50 @@
+// Package ttcn3 provides routines for evaluating TTCN-3 source code.
+//
+// This package is in alpha stage, as we are still figuring out requirements and interfaces.
+package ttcn3
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/nokia/ntt/internal/fs"
+	"github.com/nokia/ntt/internal/loc"
+	"github.com/nokia/ntt/internal/memoize"
+	"github.com/nokia/ntt/internal/ttcn3/ast"
+	"github.com/nokia/ntt/internal/ttcn3/parser"
+)
+
+var (
+	// cache stores various (expensive) calculation
+	cache = memoize.Store{}
+
+	// Limits the number of parallel parser calls per process.
+	parseLimit = make(chan struct{}, runtime.NumCPU())
+)
+
+// Tree represents the TTCN-3 syntax tree, usually of a file.
+type Tree struct {
+	fset *loc.FileSet
+	mods []*ast.Module
+	err  error
+}
+
+// ParseFile parses a file and returns a syntax tree.
+func ParseFile(path string) *Tree {
+	f := fs.Open(path)
+	f.Handle = cache.Bind(f.ID(), func(ctx context.Context) interface{} {
+		b, err := f.Bytes()
+		if err != nil {
+			return &Tree{err: err}
+		}
+
+		parseLimit <- struct{}{}
+		defer func() { <-parseLimit }()
+
+		fset := loc.NewFileSet()
+		mods, err := parser.ParseModules(fset, path, b, parser.AllErrors)
+		return &Tree{fset: fset, mods: mods, err: err}
+	})
+
+	return f.Handle.Get(context.TODO()).(*Tree)
+}
