952e942f5274724f298baec69eb4598b159abfa1
Matthias Simon <matthias.simon@nokia.com>
2021-11-17 13:32:04 +0100

    Add builtins (rnd, int2float, float2int)

--- runtime/builtin.go
+++ runtime/builtin.go
@@ -1,6 +1,9 @@
 package runtime
 
-import "math/big"
+import (
+	"math/big"
+	"math/rand"
+)
 
 var Builtins = map[string]*Builtin{
 	"lengthof": {Fn: func(args ...Object) Object {
@@ -16,6 +19,40 @@ var Builtins = map[string]*Builtin{
 
 		}
 		return Errorf("%s arguments not supported", args[0].Type())
+	}},
+
+	"rnd": {Fn: func(args ...Object) Object {
+		if len(args) != 0 {
+			return Errorf("wrong number of arguments. got=%d, want=0", len(args))
+		}
+
+		return Float{Float: big.NewFloat(rand.Float64())}
+	}},
+
+	"int2float": {Fn: func(args ...Object) Object {
+		if len(args) != 1 {
+			return Errorf("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		i, ok := args[0].(Int)
+		if !ok {
+			return Errorf("%s arguments not supported", args[0].Type())
+		}
+		return Float{Float: new(big.Float).SetInt(i.Int)}
+	}},
+
+	"float2int": {Fn: func(args ...Object) Object {
+		if len(args) != 1 {
+			return Errorf("wrong number of arguments. got=%d, want=1", len(args))
+		}
+
+		f, ok := args[0].(Float)
+		if !ok {
+			return Errorf("%s arguments not supported", args[0].Type())
+		}
+		i, _ := f.Int(nil)
+		return Int{Int: i}
+
 	}},
 }
 
