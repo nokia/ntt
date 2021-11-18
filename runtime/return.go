7ebcad9e41e99cb624622d49de3cfc2bad65ee87
Matthias Simon <matthias.simon@nokia.com>
2021-11-18 17:08:50 +0100

    Combine object files

--- runtime/return.go
+++
@@ -1,8 +0,0 @@
-package runtime
-
-type ReturnValue struct {
-	Value Object
-}
-
-func (r *ReturnValue) Type() ObjectType { return RETURN_VALUE }
-func (r *ReturnValue) Inspect() string  { return r.Value.Inspect() }
