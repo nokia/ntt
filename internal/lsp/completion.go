44565c4bb816167bb3dfaf5a880350864b2de8b6
Uwe Truetsch <uwe.truetsch@nokia.com>
2021-11-22 18:27:14 +0100

    adding better description/help for self defined functions

--- internal/lsp/completion.go
+++ internal/lsp/completion.go
@@ -1,6 +1,7 @@
 package lsp
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"path/filepath"
@@ -12,9 +13,20 @@ import (
 	"github.com/nokia/ntt/internal/lsp/protocol"
 	"github.com/nokia/ntt/internal/ntt"
 	"github.com/nokia/ntt/internal/ttcn3/ast"
+	"github.com/nokia/ntt/internal/ttcn3/printer"
 	"github.com/nokia/ntt/internal/ttcn3/token"
 	"github.com/nokia/ntt/project"
 )
+
+type FunctionDetails struct {
+	Label         string
+	Signature     string
+	Documentation string
+	HasRunsOn     bool
+	HasReturn     bool
+	HasParameters bool
+	TextFormat    protocol.InsertTextFormat
+}
 
 var (
 	moduleDefKw               = []string{"import from ", "type ", "const ", "modulepar ", "template ", "function ", "external function ", "altstep ", "testcase ", "control ", "signature "}
@@ -54,8 +66,8 @@ func newPredefinedTypes() []protocol.CompletionItem {
 	return complList
 }
 
-func getAllBehavioursFromModule(suite *ntt.Suite, kind token.Kind, mname string) []string {
-	list := make([]string, 0, 10)
+func getAllBehavioursFromModule(suite *ntt.Suite, kind token.Kind, mname string) []*FunctionDetails {
+	list := make([]*FunctionDetails, 0, 10)
 	if file, err := suite.FindModule(mname); err == nil {
 		syntax := suite.Parse(file)
 		ast.Inspect(syntax.Module, func(n ast.Node) bool {
@@ -67,7 +79,37 @@ func getAllBehavioursFromModule(suite *ntt.Suite, kind token.Kind, mname string)
 			switch node := n.(type) {
 			case *ast.FuncDecl:
 				if node.Kind.Kind == kind {
-					list = append(list, node.Name.String())
+					var sig bytes.Buffer
+					textFormat := protocol.PlainTextTextFormat
+					sig.WriteString(node.Kind.Lit + " " + mname + "." + node.Name.String())
+					len1 := len(sig.String())
+					printer.Print(&sig, syntax.FileSet, node.Params)
+					hasParams := (len(sig.String()) - len1) > 2
+					if hasParams {
+						textFormat = protocol.SnippetTextFormat
+					}
+					if node.Return != nil {
+						sig.WriteString("\n  ")
+						printer.Print(&sig, syntax.FileSet, node.Return)
+					}
+					if node.RunsOn != nil {
+						sig.WriteString("\n  ")
+						printer.Print(&sig, syntax.FileSet, node.RunsOn)
+					}
+					if node.System != nil {
+						sig.WriteString("\n  ")
+						printer.Print(&sig, syntax.FileSet, node.System)
+					}
+					tok := ast.FirstToken(node)
+
+					list = append(list, &FunctionDetails{
+						Label:         node.Name.String(),
+						HasRunsOn:     (node.Mtc != nil),
+						HasReturn:     (node.Return != nil),
+						Signature:     sig.String(),
+						Documentation: tok.Comments(),
+						HasParameters: hasParams,
+						TextFormat:    textFormat})
 				}
 				return false
 			default:
@@ -202,7 +244,7 @@ func newImportBehaviours(suite *ntt.Suite, kind token.Kind, mname string) []prot
 	items := getAllBehavioursFromModule(suite, kind, mname)
 	complList := make([]protocol.CompletionItem, 0, len(items)+1)
 	for _, v := range items {
-		complList = append(complList, protocol.CompletionItem{Label: v, Kind: protocol.FunctionCompletion})
+		complList = append(complList, protocol.CompletionItem{Label: v.Label, Kind: protocol.FunctionCompletion})
 	}
 	complList = append(complList, protocol.CompletionItem{Label: "all;", Kind: protocol.KeywordCompletion})
 	return complList
@@ -215,7 +257,7 @@ func newAllBehaviours(suite *ntt.Suite, kinds []token.Kind, mname string) []prot
 		complList := make([]protocol.CompletionItem, 0, len(files)*2)
 
 		for _, f := range files {
-			var items []string
+			var items []*FunctionDetails
 			fileName := filepath.Base(f)
 			fileName = fileName[:len(fileName)-len(filepath.Ext(fileName))]
 			if fileName != mname {
@@ -227,9 +269,14 @@ func newAllBehaviours(suite *ntt.Suite, kinds []token.Kind, mname string) []prot
 				items = append(items, getAllBehavioursFromModule(suite, kind, fileName)...)
 			}
 			for _, v := range items {
-				complList = append(complList, protocol.CompletionItem{Label: v + "()",
-					Kind: protocol.FunctionCompletion, SortText: sortPref + v,
-					Detail: fileName + "." + v + "()"})
+				insertText := v.Label + "()"
+				if v.HasParameters {
+					insertText = v.Label + "($1)$0"
+				}
+				complList = append(complList, protocol.CompletionItem{Label: v.Label + "()",
+					InsertText: insertText,
+					Kind:       protocol.FunctionCompletion, SortText: sortPref + v.Label,
+					Detail: v.Signature, Documentation: v.Documentation, InsertTextFormat: v.TextFormat})
 			}
 		}
 		return complList
@@ -443,155 +490,150 @@ func NewCompListItems(suite *ntt.Suite, pos loc.Pos, nodes []ast.Node, ownModNam
 	if nodes == nil || l == 0 {
 		return make([]protocol.CompletionItem, 0)
 	}
-	switch nodet := nodes[l-1].(type) {
-	case *ast.Ident:
-		if _, ok := nodes[0].(*ast.Module); l == 2 && ok {
-			list = newModuleDefKw()
-		}
-		if l > 2 {
-			switch scndNode := nodes[l-2].(type) {
-			case *ast.ModuleDef:
+	if isBehaviourBodyScope(nodes) {
+		list = newAllBehaviours(suite, []token.Kind{token.FUNCTION, token.ALTSTEP}, ownModName)
+		list = append(list, newPredefinedFunctions()...)
+	} else {
+		switch nodet := nodes[l-1].(type) {
+		case *ast.Ident:
+			if _, ok := nodes[0].(*ast.Module); l == 2 && ok {
 				list = newModuleDefKw()
-			case *ast.ImportDecl:
-				if scndNode.LBrace.IsValid() {
-					list = newImportkinds()
-				} else if nodet.End() >= pos {
-					// look for available modules for import
-					list = moduleNameListFromSuite(suite, ownModName, " ")
-				} else {
-					list = newImportAfterModName()
-				}
-			case *ast.DefKindExpr:
-				// happens after
-				// * the altstep/function/testcase kw while typing the identifier
-				// * inside the exception list after { while typing the kind
-				if l == 7 {
-					if _, ok := nodes[l-3].(*ast.ExceptExpr); ok {
-						if scndNode.Kind.IsValid() {
-							if impDecl, ok := nodes[l-5].(*ast.ImportDecl); ok {
-								list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
+			}
+			if l > 2 {
+				switch scndNode := nodes[l-2].(type) {
+				case *ast.ModuleDef:
+					list = newModuleDefKw()
+				case *ast.ImportDecl:
+					if scndNode.LBrace.IsValid() {
+						list = newImportkinds()
+					} else if nodet.End() >= pos {
+						// look for available modules for import
+						list = moduleNameListFromSuite(suite, ownModName, " ")
+					} else {
+						list = newImportAfterModName()
+					}
+				case *ast.DefKindExpr:
+					// happens after
+					// * the altstep/function/testcase kw while typing the identifier
+					// * inside the exception list after { while typing the kind
+					if l == 7 {
+						if _, ok := nodes[l-3].(*ast.ExceptExpr); ok {
+							if scndNode.Kind.IsValid() {
+								if impDecl, ok := nodes[l-5].(*ast.ImportDecl); ok {
+									list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
+								}
+							} else {
+								list = newImportkinds()
 							}
-						} else {
-							list = newImportkinds()
+						}
+					} else {
+						if impDecl, ok := nodes[l-3].(*ast.ImportDecl); ok {
+							list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
 						}
 					}
-				} else {
-					if impDecl, ok := nodes[l-3].(*ast.ImportDecl); ok {
-						list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
-					}
-				}
 
-			case *ast.ExceptExpr:
-				list = newImportkinds()
-			case *ast.RunsOnSpec, *ast.SystemSpec:
-				list = newAllComponentTypes(suite, " 1")
-				list = append(list, moduleNameListFromSuite(suite, ownModName, " 2")...)
-			case *ast.SelectorExpr:
-				if scndNode.X != nil {
-					switch thrdNode := nodes[l-3].(type) {
-					case *ast.RunsOnSpec, *ast.SystemSpec, *ast.ComponentTypeDecl:
-						list = newAllComponentTypesFromModule(suite, scndNode.X.LastTok().String(), " 1")
-					case *ast.TemplateDecl:
-						if thrdNode.ModifiesTok.IsValid() {
-							list = newValueDeclsFromModule(suite, scndNode.X.LastTok().String(), thrdNode.TemplateTok.Kind, false)
-						} else /*if thrdNode.Name == nil */ {
-							// NOTE: the parser produces a wrong ast under certain circumstances
-							// see: func TestTemplateModuleDotType(t *testing.T)
-							list = newAllTypesFromModule(suite, scndNode.X.LastTok().String(), "")
-						}
-					}
-				}
-			case *ast.ComponentTypeDecl:
-				// for ctrl+spc, after beginning to type an id after extends Token
-				if scndNode.ExtendsTok.LastTok().IsValid() && scndNode.Body.LBrace.Pos() > pos {
+				case *ast.ExceptExpr:
+					list = newImportkinds()
+				case *ast.RunsOnSpec, *ast.SystemSpec:
 					list = newAllComponentTypes(suite, " 1")
 					list = append(list, moduleNameListFromSuite(suite, ownModName, " 2")...)
-				}
-			case *ast.TemplateDecl:
-				if scndNode.ModifiesTok.LastTok().IsValid() && scndNode.AssignTok.Pos() > pos {
-					list = newAllValueDecls(suite, token.TEMPLATE)
-					list = append(list, moduleNameListFromSuite(suite, ownModName, "")...)
-				} else if scndNode.Name == nil {
-					list = newAllTypes(suite, ownModName)
-					list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
-				}
-			case *ast.ExprStmt:
-				if isBehaviourBodyScope(nodes) {
-					list = newAllBehaviours(suite, []token.Kind{token.FUNCTION, token.ALTSTEP}, ownModName)
-					list = append(list, newPredefinedFunctions()...)
-				}
-			}
-		}
-
-	case *ast.ImportDecl:
-		if nodet.Module == nil {
-			// look for available modules for import
-			list = moduleNameListFromSuite(suite, ownModName, " ")
-		}
-	case *ast.DefKindExpr:
-		if !nodet.Kind.IsValid() {
-			list = newImportkinds()
-		} else {
-			if impDecl, ok := nodes[l-2].(*ast.ImportDecl); ok {
-				list = newImportCompletions(suite, nodet.Kind.Kind, impDecl.Module.Tok.String())
-			} else if _, ok := nodes[l-2].(*ast.ExceptExpr); ok {
-				if impDecl, ok := nodes[l-4].(*ast.ImportDecl); ok {
-					list = newImportCompletions(suite, nodet.Kind.Kind, impDecl.Module.Tok.String())
-				}
-			}
-		}
-	case *ast.RunsOnSpec, *ast.SystemSpec:
-		list = newAllComponentTypes(suite, " 1")
-		list = append(list, moduleNameListFromSuite(suite, ownModName, " 2")...)
-	case *ast.TemplateDecl:
-		if nodet.ModifiesTok.LastTok().IsValid() && nodet.AssignTok.Pos() > pos {
-			list = newAllValueDecls(suite, token.TEMPLATE)
-			list = append(list, moduleNameListFromSuite(suite, ownModName, "")...)
-		} else if nodet.Type == nil || nodet.Name == nil {
-			list = newAllTypes(suite, ownModName)
-			list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
-		}
-	case *ast.ErrorNode:
-		// i.e. user started typing => ast.Ident might be detected instead of a kw
-		if l > 1 {
-			switch scndNode := nodes[l-2].(type) {
-			case *ast.ModuleDef:
-				// start a new module def
-				list = newModuleDefKw()
-			case *ast.DefKindExpr:
-				// NOTE: not able to reproduce this situation. Maybe it is safe to remove this code.
-				// happens streight after the altstep kw if ctrl+space is pressed
-				if impDecl, ok := nodes[l-3].(*ast.ImportDecl); ok {
-					list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
-				} else if _, ok := nodes[l-3].(*ast.ExceptExpr); ok {
-					if impDecl, ok := nodes[l-5].(*ast.ImportDecl); ok {
-						list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
+				case *ast.SelectorExpr:
+					if scndNode.X != nil {
+						switch thrdNode := nodes[l-3].(type) {
+						case *ast.RunsOnSpec, *ast.SystemSpec, *ast.ComponentTypeDecl:
+							list = newAllComponentTypesFromModule(suite, scndNode.X.LastTok().String(), " 1")
+						case *ast.TemplateDecl:
+							if thrdNode.ModifiesTok.IsValid() {
+								list = newValueDeclsFromModule(suite, scndNode.X.LastTok().String(), thrdNode.TemplateTok.Kind, false)
+							} else /*if thrdNode.Name == nil */ {
+								// NOTE: the parser produces a wrong ast under certain circumstances
+								// see: func TestTemplateModuleDotType(t *testing.T)
+								list = newAllTypesFromModule(suite, scndNode.X.LastTok().String(), "")
+							}
+						}
+					}
+				case *ast.ComponentTypeDecl:
+					// for ctrl+spc, after beginning to type an id after extends Token
+					if scndNode.ExtendsTok.LastTok().IsValid() && scndNode.Body.LBrace.Pos() > pos {
+						list = newAllComponentTypes(suite, " 1")
+						list = append(list, moduleNameListFromSuite(suite, ownModName, " 2")...)
+					}
+				case *ast.TemplateDecl:
+					if scndNode.ModifiesTok.LastTok().IsValid() && scndNode.AssignTok.Pos() > pos {
+						list = newAllValueDecls(suite, token.TEMPLATE)
+						list = append(list, moduleNameListFromSuite(suite, ownModName, "")...)
+					} else if scndNode.Name == nil {
+						list = newAllTypes(suite, ownModName)
+						list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
 					}
 				}
-			case *ast.TemplateDecl:
-				if scndNode.Type == nil || scndNode.Name == nil {
-					list = newAllTypes(suite, ownModName)
-					list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
-				}
 			}
-		}
-	case *ast.Declarator:
-		if l > 2 {
-			if valueDecl, ok := nodes[l-2].(*ast.ValueDecl); ok {
-				if valueDecl.Kind.Kind == token.PORT {
-					list = newAllPortTypes(suite, ownModName)
-					list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
-				}
-			}
-		}
-	case *ast.BlockStmt:
-		if isBehaviourBodyScope(nodes) {
-			list = newAllBehaviours(suite, []token.Kind{token.FUNCTION, token.ALTSTEP}, ownModName)
-			list = append(list, newPredefinedFunctions()...)
-		}
-	default:
-		log.Debug(fmt.Sprintf("Node not considered yet: %#v)", nodet))
 
+		case *ast.ImportDecl:
+			if nodet.Module == nil {
+				// look for available modules for import
+				list = moduleNameListFromSuite(suite, ownModName, " ")
+			}
+		case *ast.DefKindExpr:
+			if !nodet.Kind.IsValid() {
+				list = newImportkinds()
+			} else {
+				if impDecl, ok := nodes[l-2].(*ast.ImportDecl); ok {
+					list = newImportCompletions(suite, nodet.Kind.Kind, impDecl.Module.Tok.String())
+				} else if _, ok := nodes[l-2].(*ast.ExceptExpr); ok {
+					if impDecl, ok := nodes[l-4].(*ast.ImportDecl); ok {
+						list = newImportCompletions(suite, nodet.Kind.Kind, impDecl.Module.Tok.String())
+					}
+				}
+			}
+		case *ast.RunsOnSpec, *ast.SystemSpec:
+			list = newAllComponentTypes(suite, " 1")
+			list = append(list, moduleNameListFromSuite(suite, ownModName, " 2")...)
+		case *ast.TemplateDecl:
+			if nodet.ModifiesTok.LastTok().IsValid() && nodet.AssignTok.Pos() > pos {
+				list = newAllValueDecls(suite, token.TEMPLATE)
+				list = append(list, moduleNameListFromSuite(suite, ownModName, "")...)
+			} else if nodet.Type == nil || nodet.Name == nil {
+				list = newAllTypes(suite, ownModName)
+				list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
+			}
+		case *ast.ErrorNode:
+			// i.e. user started typing => ast.Ident might be detected instead of a kw
+			if l > 1 {
+				switch scndNode := nodes[l-2].(type) {
+				case *ast.ModuleDef:
+					// start a new module def
+					list = newModuleDefKw()
+				case *ast.DefKindExpr:
+					// NOTE: not able to reproduce this situation. Maybe it is safe to remove this code.
+					// happens streight after the altstep kw if ctrl+space is pressed
+					if impDecl, ok := nodes[l-3].(*ast.ImportDecl); ok {
+						list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
+					} else if _, ok := nodes[l-3].(*ast.ExceptExpr); ok {
+						if impDecl, ok := nodes[l-5].(*ast.ImportDecl); ok {
+							list = newImportCompletions(suite, scndNode.Kind.Kind, impDecl.Module.Tok.String())
+						}
+					}
+				case *ast.TemplateDecl:
+					if scndNode.Type == nil || scndNode.Name == nil {
+						list = newAllTypes(suite, ownModName)
+						list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
+					}
+				}
+			}
+		case *ast.Declarator:
+			if l > 2 {
+				if valueDecl, ok := nodes[l-2].(*ast.ValueDecl); ok {
+					if valueDecl.Kind.Kind == token.PORT {
+						list = newAllPortTypes(suite, ownModName)
+						list = append(list, moduleNameListFromSuite(suite, ownModName, " 3")...)
+					}
+				}
+			}
+		default:
+			log.Debug(fmt.Sprintf("Node not considered yet: %#v)", nodet))
+
+		}
 	}
 	return list
 }
