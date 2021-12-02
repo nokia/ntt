35d2e751691f78c1bf57822730e5d6e62b8ca2fa
Matthias Simon <5nord@users.noreply.github.com>
2021-12-02 10:47:14 +0100

    Rearrange some code (#328)

--- types/info.go
+++ types/info.go
@@ -12,11 +12,27 @@ type Info struct {
 	Scopes map[ast.Node]Scope
 }
 
-func (info *Info) Position(pos loc.Pos) loc.Position {
-	if info.fset == nil {
-		return loc.Position{}
+// TypeOf returns the type of the given expression. The given scope is stored
+// to resolve type references later.
+func (info *Info) TypeOf(n ast.Expr, scp Scope) Type {
+	if typ, ok := info.Types[n]; ok {
+		return typ
 	}
-	return info.fset.Position(pos)
+
+	switch n := n.(type) {
+	case ast.Expr:
+		// We shortcut resolving for predefined types.
+		if typ, ok := predefinedTypes[ast.Name(n)]; ok {
+			return typ
+		}
+		return &Ref{
+			Expr: n,
+			Scp:  scp,
+		}
+	}
+
+	info.trackScopes(n, scp)
+	return nil
 }
 
 // InsertTree inserts the given syntax tree n into the given parent scope scp.
@@ -59,39 +75,6 @@ func (info *Info) InsertTree(n ast.Node, scp Scope) error {
 	return &NodeNotImplementedError{Node: n}
 }
 
-func (info *Info) TypeOf(n ast.Expr, scp Scope) Type {
-	if typ, ok := info.Types[n]; ok {
-		return typ
-	}
-
-	switch n := n.(type) {
-	case ast.Expr:
-		// We shortcut resolving for predefined types.
-		if typ, ok := predefinedTypes[ast.Name(n)]; ok {
-			return typ
-		}
-		return &Ref{
-			Expr: n,
-			Scp:  scp,
-		}
-	}
-
-	info.trackScopes(n, scp)
-	return nil
-}
-
-func (info *Info) trackScopes(n ast.Node, scp Scope) {
-	if info.Scopes == nil {
-		info.Scopes = make(map[ast.Node]Scope)
-	}
-	ast.Inspect(n, func(n ast.Node) bool {
-		if id, ok := n.(*ast.Ident); ok {
-			info.Scopes[id] = scp
-		}
-		return true
-	})
-}
-
 func insertNodes(n []ast.Node, scp Scope, info *Info) *multierror.Error {
 	var errs *multierror.Error
 	for _, n := range n {
@@ -128,13 +111,36 @@ func insertDeclarator(n *ast.Declarator, typ Type, scp Scope, info *Info) error 
 		Name:  name,
 		Type:  typ,
 		Scope: scp,
-		begin: info.Position(n.Name.Pos()),
-		end:   info.Position(n.Name.End()),
+		begin: info.position(n.Name.Pos()),
+		end:   info.position(n.Name.End()),
 	}
 
 	return insert(name, obj, scp)
 }
 
+// trackScopes tracks the scopes of the given node and its children. The scope
+// is will be used to resolve references at a later stage.
+func (info *Info) trackScopes(n ast.Node, scp Scope) {
+	if info.Scopes == nil {
+		info.Scopes = make(map[ast.Node]Scope)
+	}
+	ast.Inspect(n, func(n ast.Node) bool {
+		if id, ok := n.(*ast.Ident); ok {
+			info.Scopes[id] = scp
+		}
+		return true
+	})
+}
+
+func (info *Info) position(pos loc.Pos) loc.Position {
+	if info.fset == nil {
+		return loc.Position{}
+	}
+	return info.fset.Position(pos)
+}
+
+// insert inserts the given object into the given scope. And returns an error
+// if another object with the same name already exists.
 func insert(name string, obj Object, scp Scope) error {
 	if alt := scp.Insert(name, obj); alt != nil {
 		return &RedefinitionError{Name: name, OldPos: begin(alt), NewPos: begin(obj)}
@@ -142,16 +148,18 @@ func insert(name string, obj Object, scp Scope) error {
 	return nil
 }
 
+// makeArray creates an array type from the given array definition and element type.
+func makeArray(n []*ast.ParenExpr, typ Type, scp Scope, info *Info) Type {
+	// TODO(5nord) implement list types
+	return typ
+}
+
+// begin returns the begin position of the given object.
 func begin(obj Object) loc.Position {
 	if rng, ok := obj.(Range); ok {
 		return rng.Begin()
 	}
 	return loc.Position{}
-}
-
-func makeArray(n []*ast.ParenExpr, typ Type, scp Scope, info *Info) Type {
-	// TODO(5nord) implement list types
-	return typ
 }
 
 var (
