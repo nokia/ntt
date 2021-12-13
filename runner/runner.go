1c5d1fc593c6c628649ced815e1ca04a96cbf6dd
Matthias Simon <matthias.simon@nokia.com>
2021-12-13 21:16:16 +0100

    Add docs

--- runner/runner.go
+++ runner/runner.go
@@ -4,8 +4,15 @@ import (
 	"io"
 )
 
+// The runner interface is used to run a test.
 type Runner interface {
+
+	// Run the test.
 	Run(w io.Writer, testID string) error
+
+	// LogDir returns the directory where the test logs are stored.
 	LogDir(testID string) string
+
+	// Dir returns the working directory of the test artifacts.
 	Dir() string
 }
