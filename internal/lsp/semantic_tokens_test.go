0a358bb80e6311fb530ed8e706fdf33e59a3c191
Uwe Truetsch <uwe.truetsch@nokia.com>
2022-01-21 18:49:00 +0200

    adding test for template/const declaration

--- internal/lsp/semantic_tokens_test.go
+++ internal/lsp/semantic_tokens_test.go
@@ -26,7 +26,7 @@ func TestFullModuleKwOnly(t *testing.T) {
 			port P p;
 		}
 		function f() runs on TestFunctionDefWithModuleDotRunsOn_Module_0.C0 system B0 return integer {}
-	  }`)
+	}`)
 
 	list := generateTokenList(t, suite)
 
@@ -65,4 +65,34 @@ func TestFullModuleKwTypeId(t *testing.T) {
 			3, 8, 8, uint32(lsp.Keyword), 0,
 			0, 9, 1, uint32(lsp.Function), 2,
 			0, 4, 6, uint32(lsp.Keyword), 0})
+}
+
+func TestConstAndTemplDecl(t *testing.T) {
+	suite := buildSuite(t, `module Test
+    {
+		type X Y;
+		type record MyRec {
+			integer i
+		}
+        const float pi := 3.14;
+		template MyRec a_rec1 := *;
+		template (value) MyRec a_rec2(template integer p_i) := {
+			i := p_i
+		}
+ 	  }`)
+
+	list := generateTokenList(t, suite)
+
+	assert.Equal(t, list.Data,
+		[]uint32{
+			0, 0, 6, uint32(lsp.Keyword), 0,
+			0, 7, 4, uint32(lsp.Namespace), uint32(lsp.Definition),
+			2, 2, 4, uint32(lsp.Keyword), 0,
+			0, 5, 1, uint32(lsp.Type), 0,
+			0, 2, 1, uint32(lsp.Type), uint32(lsp.Definition),
+			1, 2, 4, uint32(lsp.Keyword), 0,
+			0, 5, 6, uint32(lsp.Keyword), 0,
+			0, 7, 5, uint32(lsp.Struct), uint32(lsp.Definition),
+			1, 3, 7, uint32(lsp.Type), uint32(lsp.DefaultLibrary),
+			2, 8, 5, uint32(lsp.Keyword), 0})
 }
