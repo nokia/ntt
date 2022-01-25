0e98e7657417a7302c3de8c61316d8155a3a8430
Matthias Simon <matthias.simon@nokia.com>
2022-01-25 17:36:27 +0100

    Move test process into background

--- internal/lsp/controller.go
+++ internal/lsp/controller.go
@@ -4,19 +4,33 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"sync"
 
 	"github.com/nokia/ntt/internal/fs"
-	"github.com/nokia/ntt/internal/log"
 	"github.com/nokia/ntt/project"
 	"github.com/nokia/ntt/runner/k3s"
 )
 
 type TestController struct {
-	events chan Event
+	events  chan Event
+	tests   map[pair]*Test
+	testsMu sync.Mutex
 }
 
 type Event struct {
 	Type string
+	Test *Test
+}
+
+type Test struct {
+	pair
+	logger io.Writer
+	state  string
+}
+
+type pair struct {
+	name string
+	p    project.Interface
 }
 
 func (c *TestController) Start() error {
@@ -33,48 +47,67 @@ func (c *TestController) Shutdown() error {
 func (c *TestController) handleEvents() {
 	for event := range c.events {
 		switch event.Type {
-		case "tcst":
-			log.Println("tcst")
-		case "tcfi":
-			log.Println("tcfi")
-		case "error":
-			log.Println("error")
-		case "log":
-			log.Println("log")
+		case "tcfi", "error":
+			c.testsMu.Lock()
+			defer c.testsMu.Unlock()
+			delete(c.tests, event.Test.pair)
 		}
 	}
 }
 
 func (c *TestController) IsRunning(p project.Interface, name string) bool {
-	return false
+	c.testsMu.Lock()
+	defer c.testsMu.Unlock()
+	_, ok := c.tests[pair{name, p}]
+	return ok
 }
 
 func (c *TestController) RunTest(p project.Interface, name string, logger io.Writer) error {
-	fmt.Fprintf(logger, `
+
+	if c.IsRunning(p, name) {
+		return fmt.Errorf("test %s already running", name)
+	}
+
+	tst := &Test{
+		state:  "pending",
+		pair:   pair{name, p},
+		logger: logger,
+	}
+
+	c.testsMu.Lock()
+	c.tests[pair{name, p}] = tst
+	c.testsMu.Unlock()
+
+	go func() {
+		fmt.Fprintf(logger, `
 ===============================================================================
 Compiling test %s in %q`, name, p.Root())
 
-	r, err := k3s.New(logger, p)
-	if err != nil {
-		fmt.Fprintln(logger, err.Error())
-		return err
-	}
+		r, err := k3s.New(logger, p)
+		if err != nil {
+			fmt.Fprintln(logger, err.Error())
+			c.events <- Event{Type: "error", Test: tst}
+			return
+		}
 
-	fmt.Fprintf(logger, `
+		fmt.Fprintf(logger, `
 ===============================================================================
 Running test %s in %q`, name, p.Root())
 
-	err = r.Run(logger, name)
+		err = r.Run(logger, name)
 
-	// Show a directory listing of the artifacts (independently of any test errors)
-	logDir := r.LogDir(name)
-	if files := fs.Abs(fs.FindFilesRecursive(logDir)...); len(files) > 0 {
-		fmt.Fprintf(logger, `
+		// Show a directory listing of the artifacts (independently of any test errors)
+		logDir := r.LogDir(name)
+		if files := fs.Abs(fs.FindFilesRecursive(logDir)...); len(files) > 0 {
+			fmt.Fprintf(logger, `
 Content of log directory %q:
 ===============================================================================
 %s\n\n`,
-			logDir, strings.Join(files, "\n"))
-	}
+				logDir, strings.Join(files, "\n"))
+		}
+
+		c.events <- Event{Type: "tcfi", Test: tst}
+	}()
 
 	return nil
 }
