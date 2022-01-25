f838fbef84528df3a132f8701fcb57b70077b641
Matthias Simon <5nord@users.noreply.github.com>
2022-01-25 13:49:06 +0100

    Rearrange run test code (#352)

--- internal/lsp/commands.go
+++ internal/lsp/commands.go
@@ -2,14 +2,9 @@ package lsp
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
-	"github.com/nokia/ntt/internal/fs"
 	"github.com/nokia/ntt/internal/loc"
-	"github.com/nokia/ntt/internal/log"
 	"github.com/nokia/ntt/internal/lsp/protocol"
-	"github.com/nokia/ntt/runner/k3s"
 )
 
 // NewCommand returns a CodeLens command.
@@ -46,46 +41,16 @@ func (s *Server) executeCommand(ctx context.Context, params *protocol.ExecuteCom
 		if err := unmarshalRaw(params.Arguments, &param); err != nil {
 			return nil, err
 		}
-		return nil, nttTest(s, param.URI, param.ID)
+
+		// A file might belong to multiple test suites, possibly. But
+		// it's sufficient to just take the first test suite available,
+		// practically.
+		suite, err := s.FirstSuite(param.URI)
+		if err != nil {
+			return nil, err
+		}
+
+		return nil, s.testCtrl.RunTest(suite, param.ID, s)
 	}
 	return nil, nil
-}
-
-func nttTest(s *Server, fileURI string, testID string) error {
-
-	// A file might belong to multiple test suites, possibly. But
-	// it's sufficient to just take the first test suite available,
-	// practically.
-	suite, err := s.FirstSuite(fileURI)
-	if err != nil {
-		return err
-	}
-
-	log.Printf(`
-===============================================================================
-Compiling test %s in %q`, testID, suite.Root())
-
-	r, err := k3s.New(s, suite)
-	if err != nil {
-		s.Log(context.TODO(), err.Error())
-		return err
-	}
-
-	log.Printf(`
-===============================================================================
-Running test %s in %q`, testID, suite.Root())
-
-	err = r.Run(s, testID)
-
-	// Show a directory listing of the artifacts (independently of any test errors)
-	logDir := r.LogDir(testID)
-	if files := fs.Abs(fs.FindFilesRecursive(logDir)...); len(files) > 0 {
-		s.Log(context.Background(), fmt.Sprintf(`
-Content of log directory %q:
-===============================================================================
-%s\n\n`,
-			logDir, strings.Join(files, "\n")))
-	}
-
-	return err
 }
