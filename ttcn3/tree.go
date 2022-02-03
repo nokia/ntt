0586af61805b1a36b3003d1c3cb99d8965c58dc1
Matthias Simon <matthias.simon@nokia.com>
2022-02-03 13:07:59 +0100

    asd

--- ttcn3/tree.go
+++ ttcn3/tree.go
@@ -105,3 +105,61 @@ func (t *Tree) ModulePars() []*ast.Declarator {
 	})
 	return nodes
 }
+
+// Pos encodes a line and column tuple into a offset-based Pos tag. If file nas
+// not been parsed yet, Pos will return loc.NoPos.
+func (tree *Tree) Pos(line int, column int) loc.Pos {
+	if tree.FileSet == nil {
+		return loc.NoPos
+	}
+
+	// We asume every FileSet has only one file, to make this work.
+	return loc.Pos(int(tree.FileSet.File(loc.Pos(1)).LineStart(line)) + column - 1)
+}
+
+func (tree *Tree) SliceAt(line, col int) []ast.Node {
+	pos := tree.Pos(line, col)
+	return slice(tree.Root, pos)
+}
+
+func slice(n ast.Node, pos loc.Pos) []ast.Node {
+	var (
+		path  []ast.Node
+		found bool
+	)
+
+	ast.Inspect(n, func(n ast.Node) bool {
+		if found {
+			return false
+		}
+
+		if n == nil {
+			path = path[:len(path)-1]
+			return false
+		}
+
+		path = append(path, n)
+
+		// We don't need to descend any deeper if we're not near desired
+		// position.
+		if n.End() < pos || pos < n.Pos() {
+			return false
+		}
+
+		if n.Pos() <= pos && pos <= n.End() {
+			found = true
+		}
+
+		return !found
+	})
+
+	if len(path) == 0 {
+		return nil
+	}
+
+	// Reverse path so leaf is first element.
+	for i := 0; i < len(path)/2; i++ {
+		path[i], path[len(path)-1-i] = path[len(path)-1-i], path[i]
+	}
+	return path
+}
