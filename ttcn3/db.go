14dd3b145fd65232e24bf5177648b9032e79ce69
Matthias Simon <matthias.simon@nokia.com>
2022-02-03 17:34:23 +0100

    Use maps

--- ttcn3/db.go
+++ ttcn3/db.go
@@ -11,19 +11,19 @@ import (
 // DB implements database for querying TTCN-3 source code bases.
 type DB struct {
 	// Names is a map from symbol name to a list of files that contain the symbol.
-	Names map[string][]string
+	Names map[string]map[string]bool
 
 	// Dependencies is a map from module name to a list of modules that the module depends on.
-	Dependencies map[string][]string
+	Dependencies map[string]map[string]bool
 }
 
 // Index parses TTCN-3 source files and adds names and dependencies to the database.
 func (db *DB) Index(files ...string) {
 	if db.Names == nil {
-		db.Names = make(map[string][]string)
+		db.Names = make(map[string]map[string]bool)
 	}
 	if db.Dependencies == nil {
-		db.Dependencies = make(map[string][]string)
+		db.Dependencies = make(map[string]map[string]bool)
 	}
 
 	var (
@@ -44,12 +44,12 @@ func (db *DB) Index(files ...string) {
 					switch n := n.Def.(type) {
 					case *ast.ValueDecl:
 						for _, d := range n.Decls {
-							db.addDefinition(path, tree, m, d)
+							db.addDefinition(path, ast.Name(d))
 						}
 					case *ast.ImportDecl:
 						db.addDependency(ast.Name(m), ast.Name(n.Module))
 					default:
-						db.addDefinition(path, tree, m, n)
+						db.addDefinition(path, ast.Name(n))
 					}
 					mu.Unlock()
 					return true
@@ -62,10 +62,15 @@ func (db *DB) Index(files ...string) {
 }
 
 func (db *DB) addDependency(from, to string) {
-	db.Dependencies[from] = append(db.Dependencies[from], to)
+	if db.Dependencies[from] == nil {
+		db.Dependencies[from] = make(map[string]bool)
+	}
+	db.Dependencies[from][to] = true
 }
 
-func (db *DB) addDefinition(file string, tree *Tree, m *ast.Module, n ast.Node) {
-	name := ast.Name(n)
-	db.Names[name] = append(db.Names[name], file)
+func (db *DB) addDefinition(file string, name string) {
+	if db.Names[name] == nil {
+		db.Names[name] = make(map[string]bool)
+	}
+	db.Names[name][file] = true
 }
