c84e075d7d481ac5ef25b8c9508775d3d53beeb4
Matthias Simon <matthias.simon@nokia.com>
2022-02-04 09:39:23 +0100

    Add initial lookup

--- ttcn3/db.go
+++ ttcn3/db.go
@@ -4,8 +4,10 @@ import (
 	"sync"
 	"time"
 
+	"github.com/nokia/ntt/internal/loc"
 	"github.com/nokia/ntt/internal/log"
 	"github.com/nokia/ntt/ttcn3/ast"
+	"github.com/nokia/ntt/ttcn3/token"
 )
 
 // DB implements database for querying TTCN-3 source code bases.
@@ -15,6 +17,31 @@ type DB struct {
 
 	// Dependencies is a map from module name to a list of modules that the module depends on.
 	Dependencies map[string]map[string]bool
+}
+
+func (db *DB) ResolveAt(file string, line int, col int) {
+	start := time.Now()
+	log.Debugf("%s:%d:%d: Resolving...\n", file, line, col)
+	defer log.Debugf("%s:%d:%d: Resolve took %s\n", file, line, col, time.Since(start))
+
+	tree := ParseFile(file)
+	name, stack := getResolveStack(tree, tree.Pos(line, col))
+	if name == "" {
+		log.Printf("%s:%d:%d: No symbol to resolve.\n", file, line, col)
+		return
+	}
+
+	for len(stack) > 0 {
+		// Pop first element from the stack.
+		n := stack[0]
+		stack = stack[1:]
+
+		scope := NewScope(n)
+		if scope == nil {
+			continue
+		}
+
+	}
 }
 
 // Index parses TTCN-3 source files and adds names and dependencies to the database.
@@ -73,4 +100,17 @@ func (db *DB) addDefinition(file string, name string) {
 		db.Names[name] = make(map[string]bool)
 	}
 	db.Names[name][file] = true
+}
+
+// getResolveStack returns the symbol name and the stack of nodes that lead to the symbol.
+func getResolveStack(tree *Tree, pos loc.Pos) (string, []ast.Node) {
+	// First two elements on the stack are expected to be the ID-Token and the
+	// Identifier node.
+	if s := tree.SliceAt(pos); len(s) >= 2 {
+		if tok, ok := s[0].(*ast.Token); ok && tok.Kind == token.IDENT {
+			return tok.Lit, s[2:]
+		}
+	}
+
+	return "", nil
 }
