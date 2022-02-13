c1e8523d5306ae4df63b149b15069b6ec4dace90
Matthias Simon <5nord@users.noreply.github.com>
2022-02-13 09:59:21 +0100

    Refactor and documentation (#404)

--- ttcn3/db.go
+++ ttcn3/db.go
@@ -1,14 +1,11 @@
 package ttcn3
 
 import (
-	"fmt"
 	"sync"
 	"time"
 
-	"github.com/nokia/ntt/internal/loc"
 	"github.com/nokia/ntt/internal/log"
 	"github.com/nokia/ntt/ttcn3/ast"
-	"github.com/nokia/ntt/ttcn3/token"
 )
 
 // DB implements database for querying TTCN-3 source code bases.
@@ -120,51 +117,4 @@ func (db *DB) addDefinition(file string, name string) {
 		db.Names[name] = make(map[string]bool)
 	}
 	db.Names[name][file] = true
-}
-
-// parentNodes returns the symbol name and the stack of nodes that lead to the symbol.
-func parentNodes(tree *Tree, pos loc.Pos) (ast.Expr, []ast.Node) {
-	// First two elements on the parents are expected to be the ID-Token and the
-	// Identifier node.
-	if s := tree.SliceAt(pos); len(s) >= 2 {
-		if tok, ok := s[0].(ast.Token); ok && tok.Kind == token.IDENT {
-			return s[1].(ast.Expr), s[2:]
-		}
-	}
-
-	return nil, nil
-}
-
-// A selector expression requires a different context to resolve a symbol.
-func isSelectorID(id ast.Node, parents []ast.Node) bool {
-	if len(parents) > 0 {
-		if x, ok := parents[0].(*ast.SelectorExpr); ok {
-			return id == x.Sel
-		}
-	}
-	return false
-}
-
-// isFieldID returns true if the ID is the left hand side of a field assignment.
-func isFieldID(n ast.Node, parents []ast.Node) bool {
-	if len(parents) < 2 {
-		return false
-	}
-
-	switch parents[1].(type) {
-	case *ast.CompositeLiteral:
-		return false
-	}
-	if x, ok := parents[0].(*ast.BinaryExpr); ok {
-		return n == x.X
-	}
-	return false
-}
-
-func nodes(nodes []ast.Node) []string {
-	var types []string
-	for _, n := range nodes {
-		types = append(types, fmt.Sprintf("%T", n))
-	}
-	return types
 }
--- ttcn3/db_test.go
+++ ttcn3/db_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/nokia/ntt/internal/fs"
-	"github.com/nokia/ntt/internal/loc"
 	"github.com/nokia/ntt/ttcn3"
 )
 
@@ -45,7 +44,7 @@ func TestIndex(t *testing.T) {
 
 }
 
-func TestFindImportedDefinitions(t *testing.T) {
+func TestFindVisibleModules(t *testing.T) {
 
 	t.Run("empty", func(t *testing.T) {
 		db := ttcn3.DB{}
@@ -80,124 +79,4 @@ func TestFindImportedDefinitions(t *testing.T) {
 		}
 	})
 
-}
-
-func TestLookup(t *testing.T) {
-	tests := []struct {
-		name  string
-		input string
-		want  []string
-	}{
-		{
-			name:  "simple",
-			input: `module M {var integer x := ¶x}`,
-			want:  []string{"x0"}},
-		{
-			name:  "duplicates",
-			input: `module x {type x ¶x}`,
-			want:  []string{"x0", "x2"}},
-		{
-			name: "multi",
-			input: `module M {type integer x}
-				module M {type x      ¶x}`,
-			want: []string{"x0", "x2"}},
-		{
-			name: "imports",
-			input: `module M2 {type integer x}
-				module M1 {type ¶x y}`,
-			want: []string{}},
-		{
-			name: "imports",
-			input: `module M2 {type integer x}
-				module M1 {type ¶x y}
-				module M1 {import from M2 all}`,
-			want: []string{}},
-		{
-			name: "imports",
-			input: `module M2 {type integer x}
-				module M1 {type ¶x y; import from M2 all}`,
-			want: []string{"x0"}},
-		{
-			name:  "imports",
-			input: `module M1 {type ¶x integer; import from x all}`,
-			want:  []string{"x1"}},
-		{
-			name:  "imports",
-			input: `module M2 {} module M1 {import from ¶M2 all}`,
-			want:  []string{"M20", "M21"}},
-		{
-			name: "imports",
-			input: `module M1 {type ¶x integer}
-				module M1 {import from x all}`,
-			want: []string{}},
-		{
-			name:  "imports",
-			input: `module x {type ¶x integer; import from x all}`,
-			want:  []string{"x0", "x2"}},
-		{
-			name:  "imports",
-			input: `module x {} module x {import from x all; var integer x := ¶x}`,
-			want:  []string{"x0", "x1", "x2", "x3"}},
-		{
-			name:  "dot",
-			input: `module M {var integer x := ¶M.x}`,
-			want:  []string{"M0"}},
-		{
-			name:  "dot",
-			input: `module M {var integer x := M.¶x}`,
-			want:  []string{"x0"}},
-		{
-			name:  "dot",
-			input: `module M {type record R {int x}; type R.¶x x}`,
-			want:  []string{"x0"}},
-		{
-			name:  "dot",
-			input: `module R {type record R {int x}; type R.¶x x}`,
-			want:  []string{"x0", "x2"}},
-		{
-			name:  "dot",
-			input: `module R {type record R {int x}; type R.R.¶x x}`,
-			want:  []string{"x0"}},
-		{
-			name: "dot imports",
-			input: `module M2 {type record R {int x}}
-				module M1 {type R.¶x x; import from M2 all}`,
-			want: []string{"x0"}},
-		{
-			name: "dot imports",
-			input: `module M2 {type record R {int x}}
-				module R  {type M2.R.¶x x; import from M2 all}`,
-			want: []string{"x0"}},
-		{
-			name: "dot ambigious",
-			input: `module M2 {type record R {int x}}
-				module R  {type R.¶x x; import from M2 all}`,
-			want: []string{"x0", "x2"}},
-		{
-			name:  "typeref",
-			input: `module M { type record R {int x}; control { var M.R r; r.¶x := 23 }}`,
-			want:  []string{"x0"}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			var cursor loc.Pos
-			cursor, tt.input = extractCursor(tt.input)
-
-			tree := parseFile(t, t.Name(), tt.input)
-			ids := enumerateIDs(tree.Root)
-
-			db := &ttcn3.DB{}
-			db.Index(tree.Filename())
-
-			actual := []string{}
-			n := tree.ExprAt(cursor)
-			for _, d := range tree.LookupWithDB(n, db) {
-				actual = append(actual, ids[d.Ident])
-			}
-
-			if !equal(actual, tt.want) {
-				t.Errorf("Mismatch:\n\twant=%v,\n\t got=%v", tt.want, actual)
-			}
-		})
-	}
 }
