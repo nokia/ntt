54b6968878842ed77b8f888da25d8e8d848f11a5
Matthias Simon <5nord@users.noreply.github.com>
2022-04-04 21:32:49 +0200

    Add fake suite (#521)

--- internal/lsp/suites.go
+++ internal/lsp/suites.go
@@ -13,6 +13,68 @@ import (
 	"github.com/nokia/ntt/ttcn3"
 )
 
+type Suite struct {
+	*ntt.Suite
+	DB        *ttcn3.DB
+	modulesMu sync.Mutex
+	modules   map[string]string
+}
+
+func (s *Suite) AddSources(srcs ...string) {
+	s.Suite.AddSources(srcs...)
+	s.DB.Index(srcs...)
+}
+
+func (s *Suite) FindModule(name string) (string, error) {
+	var files []string
+	for file := range s.DB.Modules[name] {
+		if project.ContainsFile(s, file) {
+			files = append(files, file)
+		}
+	}
+
+	if len(files) == 0 {
+		log.Debugf("no such module %q. Falling back to legacy implementation\n", name)
+		return s.guessModuleByFileName(name)
+	}
+
+	if len(files) > 1 {
+		log.Debugf("warning: multiple modules with name %q: %v\n", name, files)
+	}
+
+	return files[0], nil
+}
+
+func (s *Suite) guessModuleByFileName(name string) (string, error) {
+	s.modulesMu.Lock()
+	defer s.modulesMu.Unlock()
+
+	if s.modules == nil {
+		s.modules = make(map[string]string)
+		for _, file := range project.FindAllFiles(s) {
+			name := fs.Stem(file)
+			s.modules[name] = file
+		}
+	}
+	if file, ok := s.modules[name]; ok {
+		return file, nil
+	}
+
+	// Use NTT_CACHE to locate file
+	if f := fs.Open(name + ".ttcn3").Path(); fs.IsRegular(f) {
+		s.modules[name] = f
+		return f, nil
+	}
+	return "", fmt.Errorf("no such module %q", name)
+}
+
+func NewSuite() *Suite {
+	return &Suite{
+		Suite: &ntt.Suite{},
+		DB:    &ttcn3.DB{},
+	}
+}
+
 // Suites implements the mapping between ttcn3 source files and multiple test
 // suites.
 //
@@ -30,14 +92,14 @@ import (
 // various other inputs as heuristic.
 type Suites struct {
 	mu    sync.Mutex
-	roots map[string]*ntt.Suite
+	roots map[string]*Suite
 
 	db ttcn3.DB
 }
 
 // FirstSuite returns the first test suite owning the file or an error if not
 // owning suite was found.
-func (s *Suites) FirstSuite(uri string) (*ntt.Suite, error) {
+func (s *Suites) FirstSuite(uri string) (*Suite, error) {
 	if suites := s.Owners(protocol.DocumentURI(uri)); len(suites) > 0 {
 		return suites[0], nil
 	}
@@ -45,11 +107,11 @@ func (s *Suites) FirstSuite(uri string) (*ntt.Suite, error) {
 }
 
 // Owners returns all Suites that require the opened file.
-func (s *Suites) Owners(uri protocol.DocumentURI) []*ntt.Suite {
+func (s *Suites) Owners(uri protocol.DocumentURI) []*Suite {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
-	var ret []*ntt.Suite
+	var ret []*Suite
 	for _, suite := range s.roots {
 		if project.ContainsFile(suite, string(uri.SpanURI())) {
 			ret = append(ret, suite)
@@ -67,13 +129,14 @@ func (s *Suites) AddSuite(root project.Suite) {
 	// a newly saved File
 
 	if s.roots == nil {
-		s.roots = make(map[string]*ntt.Suite)
+		s.roots = make(map[string]*Suite)
 	}
 
 	log.Printf("Adding %q to list of known test suites\n", root)
-	suite := &ntt.Suite{}
+	suite := NewSuite()
 	suite.SetRoot(root.RootDir)
 	suite.SetSourceDir(root.SourceDir)
+	suite.DB = &s.db
 	s.roots[root.RootDir] = suite
 	files, _ := suite.Files()
 	s.mu.Unlock()
