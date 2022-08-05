054c20cfbbe729124b0dcdb571f0e95372b9f081
Uwe Truetsch <uwe.truetsch@nokia.com>
2022-08-05 18:14:33 +0300

    get comments attached to the object definition

--- internal/lsp/hover.go
+++ internal/lsp/hover.go
@@ -2,30 +2,35 @@ package lsp
 
 import (
 	"context"
-	"fmt"
-	"github.com/nokia/ntt/internal/log"
+
 	"github.com/nokia/ntt/internal/lsp/protocol"
 	"github.com/nokia/ntt/ttcn3"
 	"github.com/nokia/ntt/ttcn3/ast"
 )
 
-func (s *Server) hover(ctx context.Context, params *protocol.HoverParams) (protocol.Hover, error) {
+func (s *Server) hover(ctx context.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
 	var (
-		file = string(params.TextDocument.URI.SpanURI())
-		line = int(params.Position.Line) + 1
-		col  = int(params.Position.Character) + 1
+		file    = string(params.TextDocument.URI.SpanURI())
+		line    = int(params.Position.Line) + 1
+		col     = int(params.Position.Character) + 1
+		comment string
 	)
 
 	tree := ttcn3.ParseFile(file)
 	x := tree.ExprAt(tree.Pos(line, col))
 	if x == nil {
-		log.Debug(fmt.Sprintf("No expression at %s:%d:%d\n", file, line, col))
+		return nil, nil
+	}
+	for _, def := range tree.LookupWithDB(x, &s.db) {
+		if firstTok := ast.FirstToken(def.Node); firstTok == nil {
+			continue
+		} else {
+			comment = firstTok.Comments()
+		}
 	}
 
-	comment := ast.FirstToken(x).Comments()
-
-	hoverContents := protocol.MarkupContent{Kind: "Plaintext", Value: comment}
-	hover := protocol.Hover{Contents: hoverContents}
+	hoverContents := protocol.MarkupContent{Kind: "plaintext", Value: comment}
+	hover := &protocol.Hover{Contents: hoverContents}
 
 	return hover, nil
 }
