bba148eb8e3b24a4419f4ba237154bb50b417c13
Nico Blaser <nico.blaser@nokia.com>
2022-08-16 12:05:14 +0200

    Handle Record Fields individuially

--- builtins/match.go
+++ builtins/match.go
@@ -12,5 +12,26 @@ func match(a, b runtime.Object) (bool, error) {
 		return false, runtime.Errorf("type mismatch: %s != %s", a.Type(), b.Type())
 	}
 
+	if r, ok := b.(*runtime.Record); ok {
+		return matchRecord(a.(*runtime.Record), r)
+	}
+
 	return a.Equal(b), nil
+}
+
+func matchRecord(a, b *runtime.Record) (bool, error) {
+	if len(b.Fields) != len(a.Fields) {
+		return false, runtime.Errorf("Records don't have equal amounts of Fields")
+	}
+	for k, y := range b.Fields {
+		if x, ok := a.Fields[k]; ok {
+			if ret, err := match(x, y); !ret {
+				return false, err
+			}
+		} else {
+			return false, runtime.Errorf("Value mismatch: field %s in second Record not found in first", k)
+
+		}
+	}
+	return true, nil
 }
