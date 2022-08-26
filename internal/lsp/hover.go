6b4142a23584f24b757178875ec5c5cc3261af7b
Uwe Truetsch <uwe.truetsch@nokia.com>
2022-08-26 16:56:28 +0300

    add function/altstep/testcase signatures to the hover feature

--- internal/lsp/hover.go
+++ internal/lsp/hover.go
@@ -1,20 +1,45 @@
 package lsp
 
 import (
+	"bytes"
 	"context"
 	"strings"
 
 	"github.com/nokia/ntt/internal/lsp/protocol"
 	"github.com/nokia/ntt/ttcn3"
 	"github.com/nokia/ntt/ttcn3/ast"
+	"github.com/nokia/ntt/ttcn3/printer"
 )
+
+func getSignature(def *ttcn3.Definition) string {
+	var sig bytes.Buffer
+	switch node := def.Node.(type) {
+	case *ast.FuncDecl:
+		sig.WriteString(node.Kind.Lit + " " + node.Name.String())
+		printer.Print(&sig, def.FileSet, node.Params)
+		if node.RunsOn != nil {
+			sig.WriteString("\n  ")
+			printer.Print(&sig, def.FileSet, node.RunsOn)
+		}
+		if node.System != nil {
+			sig.WriteString("\n  ")
+			printer.Print(&sig, def.FileSet, node.System)
+		}
+		if node.Return != nil {
+			sig.WriteString("\n  ")
+			printer.Print(&sig, def.FileSet, node.Return)
+		}
+	}
+	return sig.String()
+}
 
 func (s *Server) hover(ctx context.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
 	var (
-		file    = string(params.TextDocument.URI.SpanURI())
-		line    = int(params.Position.Line) + 1
-		col     = int(params.Position.Character) + 1
-		comment string
+		file      = string(params.TextDocument.URI.SpanURI())
+		line      = int(params.Position.Line) + 1
+		col       = int(params.Position.Character) + 1
+		comment   string
+		signature string
 	)
 
 	tree := ttcn3.ParseFile(file)
@@ -28,10 +53,11 @@ func (s *Server) hover(ctx context.Context, params *protocol.HoverParams) (*prot
 		} else {
 			// make line breaks conform to markdown spec
 			comment = strings.ReplaceAll(firstTok.Comments(), "\n", "  \n")
+			signature = getSignature(def) + "\n"
 		}
 	}
 
-	hoverContents := protocol.MarkupContent{Kind: "markdown", Value: comment}
+	hoverContents := protocol.MarkupContent{Kind: "markdown", Value: "```typescript\n" + string(signature) + "```\n - - -\n" + comment}
 	hover := &protocol.Hover{Contents: hoverContents}
 
 	return hover, nil
