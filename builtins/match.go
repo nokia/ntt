9674af1ecdbc332d18440382f9854a61ceb4d4b4
Nico Blaser <nico.blaser@nokia.com>
2022-09-02 12:55:42 +0200

    Add String support

--- builtins/match.go
+++ builtins/match.go
@@ -30,6 +30,14 @@ func match(a, b runtime.Object) (bool, error) {
 		default:
 			return matchRecordOf(a.(*runtime.List), b)
 		}
+	case *runtime.String:
+		if c := a.(*runtime.String); c.Len() == 1 && b.Len() == 1 {
+			if b.Value[0] == rune('?') || b.Value[0] == rune('*') || b.Value[0] == c.Value[0] {
+				return true, nil
+			}
+			return false, runtime.Errorf("tried to match %d with %d", c.Value[0], b.Value[0])
+		}
+		return matchRecordOf(a.(*runtime.String), b)
 	default:
 		return a.Equal(b), nil
 	}
@@ -57,7 +65,7 @@ func matchRecordOf(val, pat sliceHolder) (bool, error) {
 	i, back_i := 0, -1
 	j, back_j := 0, -1
 	for i < val.Len() && j < pat.Len() {
-		if pat.Get(j) == runtime.AnyOrNone {
+		if pat.Get(j) == runtime.AnyOrNone || pat.Get(j).Equal(runtime.NewString("*")) {
 			j++
 			back_j = j          // Pattern Element after *
 			back_i = i          // First Value Element which could be matched with that *
@@ -89,7 +97,7 @@ func matchRecordOf(val, pat sliceHolder) (bool, error) {
 	// reached if i == len(val) || j == len(pat)
 	if val.Len() == i {
 		for ; j < pat.Len(); j++ {
-			if pat.Get(j) != runtime.AnyOrNone {
+			if pat.Get(j) != runtime.AnyOrNone && !pat.Get(j).Equal(runtime.NewString("*")) {
 				return false, runtime.Errorf("First RecordOf is entirely matched, second isn't")
 			}
 		}
