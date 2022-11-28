1b3556b5c5ab095ababd076b4698eda3880c0c32
moosq <53606936+moosq@users.noreply.github.com>
2022-11-28 08:49:04 +0100

    simplify choosing the desired executable
    Co-authored-by: Matthias Simon <5nord@users.noreply.github.com>

--- k3/k3r/k3r.go
+++ k3/k3r/k3r.go
@@ -92,17 +92,12 @@ func (t *Test) Run(ctx context.Context) <-chan tests.Event {
 			}
 			return ""
 		}
-		executable := t.Runtime
-		k3rArgs := []string{t.T3XF, "-o", t.LogFile}
-		if gdbs := os.Getenv("GDB_SERVER"); gdbs != "" {
-			if _, err := exec.LookPath("gdbserver"); err != nil {
-				events <- tests.NewErrorEvent(err)
-				return
-			}
-			executable = "gdbserver"
-			k3rArgs = append([]string{"--once", gdbs, t.Runtime}, k3rArgs...)
-		}
-		cmd := proc.CommandContext(ctx, executable, k3rArgs...)
+		cmd := proc.CommandContext(ctx, t.Runtime, t.T3XF, "-o", t.LogFile)
+		if addr := os.Getenv("GDB_SERVER"); addr != "" {
+			gdb := proc.CommandContext(ctx, "gdbserver", "--once", addr, cmd.Name)
+			gdb.Args = append(gdb.Args, cmd.Args...)
+			cmd = gdb
+		} 
 		if s := get("K3RFLAGS"); s != "" {
 			cmd.Args = append(cmd.Args, strings.Fields(s)...)
 		}
