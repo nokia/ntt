8e1516c212f75a072ed3cbb2745f5af6e9e53097
Uwe Truetsch <uwe.truetsch@nokia.com>
2023-02-07 19:14:26 +0200

    supply an error message if the sub-process exited ubnormally

--- main.go
+++ main.go
@@ -189,6 +189,9 @@ func fatal(err error) {
 	switch err := err.(type) {
 	case *exec.ExitError:
 		waitStatus := err.Sys().(syscall.WaitStatus)
+		if waitStatus.ExitStatus() == -1 {
+			errors.PrintError(os.Stderr, err)
+		}
 		os.Exit(waitStatus.ExitStatus())
 	case errors.ErrorList:
 		errors.PrintError(os.Stderr, err)
