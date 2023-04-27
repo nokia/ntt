049b38d2006c8ffad694b9a41f4364a5227c3f45
Matthias Simon <matthias.simon@nokia.com>
2023-04-27 22:30:41 +0200

    Refactor references_test.go

--- internal/lsp/references_test.go
+++ internal/lsp/references_test.go
@@ -1,57 +1,57 @@
 package lsp_test
 
 import (
-	"strings"
+	"fmt"
 	"testing"
 
+	"github.com/nokia/ntt/internal/fs"
 	"github.com/nokia/ntt/internal/lsp"
 	"github.com/nokia/ntt/internal/lsp/protocol"
 	"github.com/nokia/ntt/ttcn3"
 	"github.com/stretchr/testify/assert"
 )
 
-func StripPathFromURI(list []protocol.Location) []protocol.Location {
-	ret := make([]protocol.Location, 0, len(list))
-	for _, elem := range list {
-		fileName := elem.URI[strings.LastIndexByte(string(elem.URI), '/')+1:]
-		ret = append(ret, protocol.Location{URI: fileName, Range: elem.Range})
-	}
-	return ret
-}
-
 func TestFindAllTypeDefs(t *testing.T) {
-	suite, _ := buildSuite(t, `module Test
-	{
+	input1 := `
+	module A {
 		type integer Byte(0..255);
 		function f() return Byte {
-			var Byte ret := 100;
+		var Byte ret := 100;
 		}
-	  }`, `module A
-	  {
-			import from TestFindAllTypeDefs_Module_0 all;
-			template Byte a_byte := ?;
-	  }`, `module B
-	  {
-			import from TestFindAllTypeDefs_Module_0 {type Byte}
-			type Byte AliasByte;
-	  }`)
+	}
+	module B {
+		import from A all;
+		template Byte a_byte := ?;
+	}`
+	input2 := `
+	module C {
+		import from A {type Byte}
+		type Byte AliasByte;
+	}`
+
+	name1 := fmt.Sprintf("test://%s_input1", t.Name())
+	name2 := fmt.Sprintf("test://%s_input2", t.Name())
+
+	fs.SetContent(name1, []byte(input1))
+	fs.SetContent(name2, []byte(input2))
+
 	db := &ttcn3.DB{}
-	db.Index(suite.Files()...)
+	db.Index(name1, name2)
 
 	// Lookup `Msg`
 	list := lsp.NewAllIdsWithSameName(db, "Byte")
 
 	assert.Equal(t, []protocol.Location{
-		{URI: "TestFindAllTypeDefs_Module_0.ttcn3",
+		{URI: "test://TestFindAllTypeDefs_input1",
 			Range: protocol.Range{Start: protocol.Position{Line: 2, Character: 15}, End: protocol.Position{Line: 2, Character: 19}}},
-		{URI: "TestFindAllTypeDefs_Module_0.ttcn3",
+		{URI: "test://TestFindAllTypeDefs_input1",
 			Range: protocol.Range{Start: protocol.Position{Line: 3, Character: 22}, End: protocol.Position{Line: 3, Character: 26}}},
-		{URI: "TestFindAllTypeDefs_Module_0.ttcn3",
-			Range: protocol.Range{Start: protocol.Position{Line: 4, Character: 7}, End: protocol.Position{Line: 4, Character: 11}}},
-		{URI: "TestFindAllTypeDefs_Module_1.ttcn3",
-			Range: protocol.Range{Start: protocol.Position{Line: 3, Character: 12}, End: protocol.Position{Line: 3, Character: 16}}},
-		{URI: "TestFindAllTypeDefs_Module_2.ttcn3",
-			Range: protocol.Range{Start: protocol.Position{Line: 2, Character: 50}, End: protocol.Position{Line: 2, Character: 54}}},
-		{URI: "TestFindAllTypeDefs_Module_2.ttcn3",
-			Range: protocol.Range{Start: protocol.Position{Line: 3, Character: 8}, End: protocol.Position{Line: 3, Character: 12}}}}, StripPathFromURI(list))
+		{URI: "test://TestFindAllTypeDefs_input1",
+			Range: protocol.Range{Start: protocol.Position{Line: 4, Character: 6}, End: protocol.Position{Line: 4, Character: 10}}},
+		{URI: "test://TestFindAllTypeDefs_input1",
+			Range: protocol.Range{Start: protocol.Position{Line: 9, Character: 11}, End: protocol.Position{Line: 9, Character: 15}}},
+		{URI: "test://TestFindAllTypeDefs_input2",
+			Range: protocol.Range{Start: protocol.Position{Line: 2, Character: 22}, End: protocol.Position{Line: 2, Character: 26}}},
+		{URI: "test://TestFindAllTypeDefs_input2",
+			Range: protocol.Range{Start: protocol.Position{Line: 3, Character: 7}, End: protocol.Position{Line: 3, Character: 11}}}}, list)
 }
