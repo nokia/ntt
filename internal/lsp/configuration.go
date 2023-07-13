5d23ef21de2c6bd28d109d2e9368a152604a3073
Truetsch Uwe <uwe.truetsch@nokia.com>
2023-07-13 12:50:40 +0000

    Feature flags for experimental features

--- internal/lsp/configuration.go
+++ internal/lsp/configuration.go
@@ -6,6 +6,10 @@ import (
 	"github.com/nokia/ntt/internal/lsp/protocol"
 )
 
+const DIAGNOSTICS_CONFIG_KEY = "ttcn3.experimental.diagnostics.enabled"
+const FORMATTER_CONFIG_KEY = "ttcn3.experimental.format.enabled"
+const SEMANTIC_TOKENS_CONFIG_KEY = "ttcn3.experimental.semanticTokens.enabled"
+
 func (s *Server) Config(section string) interface{} {
 	v, err := s.client.Configuration(context.TODO(), &protocol.ParamConfiguration{
 		ConfigurationParams: protocol.ConfigurationParams{
@@ -24,8 +28,62 @@ func (s *Server) Config(section string) interface{} {
 }
 
 func (s *Server) didChangeConfiguration(ctx context.Context, _ *protocol.DidChangeConfigurationParams) error {
-	if format, ok := s.Config("ttcn3.format.enabled").(bool); ok {
-		s.serverConfig.FormatEnabled = format
+	regList := make([]protocol.Registration, 0, 3)
+	unregList := make([]protocol.Unregistration, 0, 3)
+
+	confRes, ok := s.Config(FORMATTER_CONFIG_KEY).(bool)
+	if !ok {
+		confRes = false
+	}
+	if s.clientCapability.HasDynRegForFormatter && s.serverConfig.FormatEnabled != confRes {
+		s.serverConfig.FormatEnabled = confRes
+		if confRes {
+			regList = append(regList, protocol.Registration{
+				ID:     "TEXTDOCUMENT_FORMATTING",
+				Method: "textDocument/formatting",
+				RegisterOptions: protocol.TextDocumentRegistrationOptions{
+					DocumentSelector: protocol.DocumentSelector{
+						protocol.DocumentFilter{Language: "ttcn3", Scheme: "file", Pattern: "**/*.ttcn3"},
+					},
+				}})
+		} else {
+			unregList = append(unregList, protocol.Unregistration{
+				ID:     "TEXTDOCUMENT_FORMATTING",
+				Method: "textDocument/formatting"})
+		}
+	}
+	confRes, ok = s.Config(SEMANTIC_TOKENS_CONFIG_KEY).(bool)
+	if !ok {
+		confRes = false
+	}
+	if s.clientCapability.HasDynRegForSemTok && s.serverConfig.SemantikTokensEnabled != confRes {
+		s.serverConfig.SemantikTokensEnabled = confRes
+		if confRes {
+			regList = append(regList, protocol.Registration{
+				ID:              "TEXTDOCUMENT_SEMANTICTOKENS",
+				Method:          "textDocument/semanticTokens",
+				RegisterOptions: newSemanticTokens()})
+		} else {
+			unregList = append(unregList, protocol.Unregistration{
+				ID:     "TEXTDOCUMENT_SEMANTICTOKENS",
+				Method: "textDocument/semanticTokens"})
+		}
+	}
+	confRes, ok = s.Config(DIAGNOSTICS_CONFIG_KEY).(bool)
+	if !ok {
+		confRes = false
+	}
+
+	if s.serverConfig.SemantikTokensEnabled != confRes {
+		s.serverConfig.DiagnosticsEnabled = confRes
+		// NOTE: dynamic registration of diagnostics is only available from lsp 3.17 on
+	}
+
+	if len(regList) > 0 {
+		s.client.RegisterCapability(ctx, &protocol.RegistrationParams{Registrations: regList})
+	}
+	if len(unregList) > 0 {
+		s.client.UnregisterCapability(ctx, &protocol.UnregistrationParams{Unregisterations: unregList})
 	}
 	return nil
 }
