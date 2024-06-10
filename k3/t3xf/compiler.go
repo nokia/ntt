ad4443c0c18966c9c493416fa29722bea3176c10
Matthias Simon <matthias.simon@nokia.com>
2024-06-10 12:42:21 +0200

    Add initial T3XF code generator

---
+++ k3/t3xf/compiler.go
@@ -0,0 +1,710 @@
+package t3xf
+
+import (
+	"errors"
+	"fmt"
+	"math"
+	"math/big"
+
+	"github.com/nokia/ntt/k3/t3xf/opcode"
+	"github.com/nokia/ntt/ttcn3/syntax"
+)
+
+type scope struct {
+	parent *scope
+	store  map[string]symbol
+}
+
+func newScope(parent *scope) *scope {
+	return &scope{parent: parent, store: make(map[string]symbol)}
+}
+
+func (s *scope) lookup(name string) (symbol, bool) {
+	obj, ok := s.store[name]
+	if !ok && s.parent != nil {
+		return s.parent.lookup(name)
+	}
+	return obj, ok
+}
+
+func (s *scope) insert(name string, sym symbol) {
+	s.store[name] = sym
+}
+
+type symbol struct {
+	n   syntax.Node
+	op  opcode.Opcode
+	arg any
+}
+
+type Compiler struct {
+	scope    *scope
+	err      error
+	e        *Encoder
+	lastLine int
+	fields   map[string]int
+}
+
+func NewCompiler() *Compiler {
+	return &Compiler{
+		e:      NewEncoder(),
+		fields: make(map[string]int),
+	}
+}
+
+func (c *Compiler) Err() error {
+	return c.err
+}
+
+func (c *Compiler) Assemble() ([]byte, error) {
+	if c.err == nil {
+		return c.e.Assemble()
+	}
+	return nil, c.err
+}
+
+func (c *Compiler) Compile(n syntax.Node) error {
+	if line := syntax.Begin(n).Line; line != c.lastLine {
+		c.emit(opcode.LINE, line)
+		c.lastLine = line
+	}
+
+	switch n := n.(type) {
+	case *syntax.Root:
+		if n.Filename != "" {
+			c.emit(opcode.SCAN, 0)
+		}
+		for _, child := range n.Nodes {
+			c.Compile(child)
+		}
+		if n.Filename != "" {
+			c.emit(opcode.BLOCK, 0)
+			c.emit(opcode.NAME, n.Filename)
+			c.emit(opcode.SOURCE, 0)
+		}
+
+	case *syntax.Module:
+		c.scope.insert(n.Name.String(), symbol{n: n})
+		c.scope = newScope(c.scope)
+
+		op := opcode.MODULE
+		if attrs := n.With; attrs != nil {
+			op = opcode.MODULEW
+			c.Compile(attrs)
+		}
+		c.emit(opcode.SCAN, 0)
+		for _, child := range n.Defs {
+			c.Compile(child.Def)
+		}
+		c.emit(opcode.BLOCK, 0)
+
+		c.emit(opcode.NAME, n.Name.String())
+		c.emit(op, 0)
+		c.scope = c.scope.parent
+
+	case *syntax.ModuleDef:
+		c.Compile(n.Def)
+
+	case *syntax.FuncDecl:
+		switch k := n.Kind.Kind(); {
+		case k == syntax.FUNCTION && n.External == nil:
+			c.compileFunction(n)
+		case k == syntax.FUNCTION && n.External != nil:
+			c.compileExtFunc(n)
+		case k == syntax.TESTCASE:
+			c.compileTestcase(n)
+		case k == syntax.ALTSTEP:
+			c.compileAltstep(n)
+		default:
+			c.errorf("unsupported behaviour %s", k)
+		}
+
+	case *syntax.ValueDecl:
+		k := syntax.VAR
+		if n.Kind != nil {
+			k = n.Kind.Kind()
+		}
+
+		fn := c.compileVar
+		switch k {
+		case syntax.CONST:
+			fn = c.compileConst
+		case syntax.MODULEPAR:
+			fn = c.compileModulePar
+		}
+
+		for _, decl := range n.Decls {
+			fn(k, n.TemplateRestriction, n.Modif, n.Type, decl, n.With)
+		}
+
+	case *syntax.ControlPart:
+		op := opcode.CONTROL
+		if attrs := n.With; attrs != nil {
+			op := opcode.CONTROLW
+			c.Compile(attrs)
+		}
+		c.Compile(n.Body)
+		addr := c.emit(op, 0)
+		c.scope.insert("control", symbol{n: n, op: opcode.REF, arg: Reference(addr)})
+
+	case *syntax.BlockStmt:
+		if len(n.Stmts) == 0 {
+			c.emit(opcode.SKIP, 0)
+			break
+		}
+		c.emit(opcode.SCAN, 0)
+		for _, child := range n.Stmts {
+			c.Compile(child)
+		}
+		c.emit(opcode.BLOCK, 0)
+
+	case *syntax.ReturnStmt:
+		if n.Result != nil {
+			c.Compile(n.Result)
+		}
+		c.emit(opcode.RETURN, 0)
+
+	case *syntax.IfStmt:
+		op := opcode.IF
+		c.Compile(n.Cond)
+		c.Compile(n.Then)
+		if n.Else != nil {
+			c.Compile(n.Else)
+			op = opcode.IFELSE
+		}
+		c.emit(op, 0)
+
+	case *syntax.WhileStmt:
+		c.emit(opcode.SCAN, 0)
+		c.Compile(n.Cond)
+		c.emit(opcode.BLOCK, 0)
+		c.Compile(n.Body)
+		c.emit(opcode.WHILE, 0)
+
+	case *syntax.DoWhileStmt:
+		c.Compile(n.Body)
+		c.emit(opcode.SCAN, 0)
+		c.Compile(n.Cond)
+		c.emit(opcode.BLOCK, 0)
+		c.emit(opcode.DOWHILE, 0)
+
+	case *syntax.ForStmt:
+		c.emit(opcode.SCAN, 0)
+		c.Compile(n.Init)
+		c.emit(opcode.BLOCK, 0)
+		c.emit(opcode.SCAN, 0)
+		c.Compile(n.Cond)
+		c.emit(opcode.BLOCK, 0)
+		c.emit(opcode.SCAN, 0)
+		c.Compile(n.Post)
+		c.emit(opcode.BLOCK, 0)
+		c.Compile(n.Body)
+		c.emit(opcode.FOR, 0)
+
+	case *syntax.ExprStmt:
+		c.Compile(n.Expr)
+
+	case *syntax.DeclStmt:
+		c.Compile(n.Decl)
+
+	case *syntax.CallExpr:
+		for _, arg := range n.Args.List {
+			c.Compile(arg)
+		}
+		c.Compile(n.Fun)
+
+	case *syntax.BinaryExpr:
+		c.Compile(n.X)
+		c.Compile(n.Y)
+		switch n.Op.Kind() {
+		case syntax.ADD:
+			c.emit(opcode.ADD, 0)
+		case syntax.SUB:
+			c.emit(opcode.SUB, 0)
+		case syntax.MUL:
+			c.emit(opcode.MUL, 0)
+		case syntax.DIV:
+			c.emit(opcode.DIV, 0)
+		case syntax.MOD:
+			c.emit(opcode.MOD, 0)
+		case syntax.REM:
+			c.emit(opcode.REM, 0)
+		case syntax.EQ:
+			c.emit(opcode.EQ, 0)
+		case syntax.NE:
+			c.emit(opcode.NE, 0)
+		case syntax.RANGE:
+			c.emit(opcode.RANGE, 0)
+		default:
+			c.errorf("unsupported binary operator %s", n.Op)
+		}
+
+	case *syntax.ValueLiteral:
+		switch n.Tok.Kind() {
+		case syntax.INT:
+			s := n.Tok.String()
+			bi, ok := big.NewInt(0).SetString(s, 10)
+			if !ok {
+				c.errorf("invalid integer %s", s)
+			}
+			if i := bi.Int64(); bi.IsInt64() && math.MinInt32 <= i && i <= math.MaxInt32 {
+				c.emit(opcode.NATLONG, int(i))
+			} else {
+				c.emit(opcode.ISTR, s)
+			}
+
+		case syntax.STRING:
+			s, err := syntax.Unquote(n.Tok.String())
+			if err != nil {
+				c.errorf("invalid string %s", n.Tok)
+			}
+			c.emit(opcode.UTF8, s)
+
+		case syntax.TRUE:
+			c.emit(opcode.TRUE, 0)
+		case syntax.FALSE:
+			c.emit(opcode.FALSE, 0)
+
+		case syntax.NULL:
+			c.emit(opcode.NULL, 0)
+		case syntax.ANY:
+			c.emit(opcode.ANY, 0)
+		case syntax.MUL:
+			c.emit(opcode.ANYN, 0)
+		case syntax.OMIT:
+			c.emit(opcode.OMIT, 0)
+
+		case syntax.ERROR:
+			c.emit(opcode.ERROR, 0)
+		case syntax.FAIL:
+			c.emit(opcode.FAIL, 0)
+		case syntax.INCONC:
+			c.emit(opcode.INCONC, 0)
+		case syntax.PASS:
+			c.emit(opcode.PASS, 0)
+		case syntax.NONE:
+			c.emit(opcode.NONE, 0)
+		}
+
+	case *syntax.RefSpec:
+		c.Compile(n.X)
+
+	case *syntax.Ident:
+		s := n.String()
+		switch s {
+		case "log":
+			c.emit(opcode.LOG, 0)
+		case "integer":
+			c.emit(opcode.INTEGER, 0)
+		case "float":
+			c.emit(opcode.FLOAT, 0)
+		case "bitstring":
+			c.emit(opcode.BITSTRING, 0)
+		case "hexstring":
+			c.emit(opcode.HEXSTRING, 0)
+		case "octetstring":
+			c.emit(opcode.OCTETSTRING, 0)
+		case "boolean":
+			c.emit(opcode.BOOLEAN, 0)
+		case "charstring":
+			c.emit(opcode.CHARSTRING, 0)
+		case "timer":
+			c.emit(opcode.TIMER, 0)
+		default:
+			c.errorf("unknown identifier %s", s)
+		}
+
+	case *syntax.WithSpec:
+		c.emit(opcode.SCAN, 0)
+		for _, child := range n.List {
+			c.Compile(child)
+		}
+		c.emit(opcode.BLOCK, 0)
+
+	case *syntax.WithStmt:
+		c.Compile(n.Value)
+		switch n.Kind.Kind() {
+		case syntax.EXTENSION:
+			c.emit(opcode.EXTENSION, 0)
+		case syntax.ENCODE:
+			c.emit(opcode.ENCODE, 0)
+		case syntax.VARIANT:
+			c.emit(opcode.VARIANT, 0)
+		default:
+			c.errorf("unsupported attribute %s", n.Kind)
+		}
+
+	case *syntax.FormalPars:
+		if len(n.List) == 0 {
+			c.emit(opcode.SKIP, 0)
+			break
+		}
+		c.emit(opcode.SCAN, 0)
+		for _, child := range n.List {
+			c.Compile(child)
+		}
+		c.emit(opcode.BLOCK, 0)
+
+	case *syntax.FormalPar:
+		c.Compile(n.Type)
+		if n.TemplateRestriction != nil {
+			c.Compile(n.TemplateRestriction)
+		}
+		c.emit(opcode.NAME, n.Name.String())
+		dir := opcode.IN
+		if n.Direction != nil {
+			switch n.Direction.Kind() {
+			case syntax.OUT:
+				dir = opcode.OUT
+			case syntax.INOUT:
+				dir = opcode.INOUT
+			}
+		}
+		c.emit(dir, 0)
+
+	case *syntax.RestrictionSpec:
+		op := opcode.PERMITT
+		if n.Tok != nil {
+			switch n.Tok.Kind() {
+			case syntax.OMIT:
+				op = opcode.PERMITO
+			case syntax.PRESENT:
+				op = opcode.PERMITP
+			}
+		}
+		c.emit(op, 0)
+
+	case *syntax.ReturnSpec:
+		c.Compile(n.Type)
+		if n.Restriction != nil {
+			c.Compile(n.Restriction)
+		}
+
+	case *syntax.StructTypeDecl:
+		op := opcode.TYPE
+		if n.With != nil {
+			op = opcode.TYPEW
+			c.Compile(n.With)
+		}
+		c.emit(opcode.SCAN, 0)
+		for _, field := range n.Fields {
+			c.Compile(field)
+			c.emit(opcode.NAME, field.Name.String())
+			switch {
+			case field.Optional != nil:
+				c.emit(opcode.FIELDO, 0)
+			case n.Kind.Kind() == syntax.UNION:
+				c.emit(opcode.IFIELD, c.fieldIndex(field.Name.String()))
+			default:
+				c.emit(opcode.FIELD, 0)
+			}
+		}
+		c.emit(opcode.BLOCK, 0)
+		switch n.Kind.Kind() {
+		case syntax.RECORD:
+			c.emit(opcode.RECORD, 0)
+		case syntax.SET:
+			c.emit(opcode.SET, 0)
+		case syntax.UNION:
+			c.emit(opcode.UNION, 0)
+		default:
+			c.errorf("unsupported struct type %s", n.Kind)
+		}
+		c.emit(opcode.NAME, n.Name.String())
+		c.emit(op, 0)
+
+	case *syntax.SubTypeDecl:
+		op := opcode.TYPE
+		if n.With != nil {
+			op = opcode.TYPEW
+			c.Compile(n.With)
+		}
+		c.Compile(n.Field)
+		c.emit(opcode.NAME, n.Field.Name.String())
+		c.emit(op, 0)
+
+	case *syntax.Field:
+		c.Compile(n.Type)
+
+		// NOTE: This length constraint must move to the element type. not sure how this works mit structs (inside fields)
+		if n.ValueConstraint != nil || n.LengthConstraint != nil {
+			switch {
+			case n.ValueConstraint == nil:
+				c.emit(opcode.ANY, 0)
+			case len(n.ValueConstraint.List) == 1:
+				c.Compile(n.ValueConstraint.List[0])
+			case len(n.ValueConstraint.List) > 1:
+				c.emit(opcode.MARK, 0)
+				for _, x := range n.ValueConstraint.List {
+					c.Compile(x)
+				}
+				c.emit(opcode.COLLECT, 0)
+			}
+			if n.LengthConstraint != nil {
+				for _, x := range n.LengthConstraint.Size.List {
+					c.Compile(x)
+				}
+				c.emit(opcode.LENGTH, 0)
+			}
+			c.emit(opcode.SUBTYPE, 0)
+		}
+		c.compileArrayDef(n.ArrayDef)
+
+	case *syntax.ListSpec:
+		c.Compile(n.ElemType)
+		switch n.Kind.Kind() {
+		case syntax.RECORD:
+			c.emit(opcode.RECORDOF, 0)
+		case syntax.SET:
+			c.emit(opcode.SETOF, 0)
+		default:
+			c.errorf("unsupported list type %s", n.Kind)
+
+		}
+		if n.Length != nil {
+			c.emit(opcode.SCAN, 0)
+			c.emit(opcode.ANY, 0)
+			for _, x := range n.Length.Size.List {
+				c.Compile(x)
+			}
+			c.emit(opcode.LENGTH, 0)
+			c.emit(opcode.BLOCK, 0)
+			c.emit(opcode.SUBTYPE, 0)
+		}
+
+	default:
+		c.errorf("unexpected node type %T", n)
+	}
+
+	return nil
+}
+
+func (c *Compiler) compileFunction(n *syntax.FuncDecl) error {
+	c.scope.insert(n.Name.String(), symbol{n: n})
+	c.scope = newScope(c.scope)
+
+	if n.With != nil {
+		c.errorf("function attributes not supported")
+	}
+	if n.Mtc != nil {
+		c.errorf("MTC clause not supported")
+	}
+
+	op := opcode.FUNCTION
+	switch {
+	case n.Return == nil && n.RunsOn != nil:
+		op = opcode.FUNCTIONB
+		c.Compile(n.RunsOn.Comp)
+
+	case n.Return != nil && n.RunsOn == nil:
+		op = opcode.FUNCTIONV
+		c.Compile(n.Return)
+
+	case n.Return != nil && n.RunsOn != nil:
+		op = opcode.FUNCTIONVB
+		c.Compile(n.RunsOn.Comp)
+		c.Compile(n.Return)
+	}
+
+	c.Compile(n.Params)
+	c.Compile(n.Body)
+	c.emit(opcode.NAME, n.Name.String())
+	c.emit(op, 0)
+
+	c.scope = c.scope.parent
+	return nil
+}
+
+func (c *Compiler) compileExtFunc(n *syntax.FuncDecl) error {
+	c.scope.insert(n.Name.String(), symbol{n: n})
+	c.scope = newScope(c.scope)
+
+	if n.Mtc != nil {
+		c.errorf("MTC clause not supported")
+	}
+
+	if n.RunsOn != nil {
+		c.errorf("runs on clause not supported")
+	}
+
+	op := opcode.FUNCTIONX
+	switch {
+	case n.Return == nil && n.With != nil:
+		op = opcode.FUNCTIONXW
+		c.Compile(n.With)
+
+	case n.Return != nil && n.With == nil:
+		op = opcode.FUNCTIONXV
+		c.Compile(n.Return)
+
+	case n.Return != nil && n.With != nil:
+		op = opcode.FUNCTIONXVW
+		c.Compile(n.Return)
+		c.Compile(n.With)
+	}
+	c.Compile(n.Params)
+	c.emit(opcode.NAME, n.Name.String())
+	c.emit(op, 0)
+
+	c.scope = c.scope.parent
+	return nil
+}
+
+func (c *Compiler) compileTestcase(n *syntax.FuncDecl) error {
+	c.scope.insert(n.Name.String(), symbol{n: n})
+	c.scope = newScope(c.scope)
+
+	op := opcode.TESTCASE
+	if n.System != nil {
+		op = opcode.TESTCASES
+		c.Compile(n.System.Comp)
+	}
+	c.Compile(n.RunsOn.Comp)
+	c.Compile(n.Params)
+	c.Compile(n.Body)
+	c.emit(opcode.NAME, n.Name.String())
+	c.emit(op, 0)
+
+	c.scope = c.scope.parent
+	return nil
+}
+
+func (c *Compiler) compileAltstep(n *syntax.FuncDecl) error {
+	c.scope.insert(n.Name.String(), symbol{n: n})
+	c.scope = newScope(c.scope)
+
+	if n.Mtc != nil {
+		c.errorf("MTC clause not supported")
+	}
+
+	op := opcode.ALTSTEP
+	switch {
+	case n.RunsOn == nil && n.With != nil:
+		op = opcode.ALTSTEPW
+		c.Compile(n.With)
+
+	case n.RunsOn != nil && n.With == nil:
+		op = opcode.ALTSTEPB
+		c.Compile(n.RunsOn.Comp)
+
+	case n.RunsOn != nil && n.With != nil:
+		op = opcode.ALTSTEPBW
+		c.Compile(n.RunsOn.Comp)
+		c.Compile(n.With)
+	}
+
+	c.Compile(n.Params)
+	c.Compile(n.Body)
+	c.emit(opcode.NAME, n.Name.String())
+	c.emit(op, 0)
+
+	return nil
+}
+
+func (c *Compiler) compileVar(kind syntax.Kind, restr *syntax.RestrictionSpec, modif syntax.Token, typ syntax.Expr, decl *syntax.Declarator, attrs *syntax.WithSpec) error {
+	if attrs != nil {
+		c.errorf("attributes not supported")
+	}
+	c.Compile(typ)
+	c.compileArrayDef(decl.ArrayDef)
+
+	c.emit(opcode.NAME, decl.Name.String())
+	if modif != nil {
+		c.errorf("modifiers not supported")
+	}
+	addr := c.emit(opcode.VAR, 0)
+	c.scope.insert(decl.Name.String(), symbol{n: decl, op: opcode.REF, arg: Reference(addr)})
+	if decl.Value != nil {
+		c.Compile(decl.Value)
+		c.emit(opcode.REF, Reference(addr))
+		c.emit(opcode.ASSIGN, 0)
+	}
+	return nil
+}
+
+func (c *Compiler) compileConst(kind syntax.Kind, restr *syntax.RestrictionSpec,
+	modif syntax.Token, typ syntax.Expr, decl *syntax.Declarator, attrs *syntax.WithSpec) error {
+	op := opcode.CONST
+	if attrs != nil {
+		op = opcode.CONSTW
+		c.Compile(attrs)
+	}
+
+	if decl.Value != nil {
+		c.emit(opcode.SCAN, 0)
+		c.Compile(decl.Value)
+		c.emit(opcode.BLOCK, 0)
+	} else {
+		c.errorf("constant declaration without value")
+	}
+
+	c.Compile(typ)
+	c.compileArrayDef(decl.ArrayDef)
+	c.emit(opcode.NAME, decl.Name.String())
+	if modif != nil {
+		c.errorf("modifiers not supported")
+	}
+	addr := c.emit(op, 0)
+	c.scope.insert(decl.Name.String(), symbol{n: decl, op: opcode.REF, arg: Reference(addr)})
+	return nil
+}
+
+func (c *Compiler) compileArrayDef(a []*syntax.ParenExpr) {
+	for i := len(a) - 1; i >= 0; i-- {
+		c.emit(opcode.SCAN, 0)
+		for _, x := range a[i].List {
+			c.Compile(x)
+		}
+		c.emit(opcode.BLOCK, 0)
+		c.emit(opcode.ARRAY, 0)
+	}
+}
+
+func (c *Compiler) compileModulePar(kind syntax.Kind, restr *syntax.RestrictionSpec,
+	modif syntax.Token, typ syntax.Expr, decl *syntax.Declarator, attrs *syntax.WithSpec) error {
+
+	op := opcode.MPAR
+	if decl.Value != nil {
+		op = opcode.MPARD
+		c.emit(opcode.SCAN, 0)
+		c.Compile(decl.Value)
+		c.emit(opcode.BLOCK, 0)
+	}
+
+	if attrs != nil {
+		c.errorf("attributes not supported")
+	}
+	if modif != nil {
+		c.errorf("modifiers not supported")
+	}
+
+	c.Compile(typ)
+	c.compileArrayDef(decl.ArrayDef)
+
+	c.emit(opcode.NAME, decl.Name.String())
+	addr := c.emit(op, 0)
+	c.scope.insert(decl.Name.String(), symbol{n: decl, op: opcode.REF, arg: Reference(addr)})
+	return nil
+}
+
+func (c *Compiler) emit(op opcode.Opcode, arg any) int {
+	pos := c.e.Len()
+	if err := c.e.Encode(op, arg); err != nil {
+		c.errorf("%w", err)
+	}
+	return pos
+
+}
+
+func (c *Compiler) errorf(format string, args ...interface{}) {
+	c.err = errors.Join(c.err, fmt.Errorf(format, args...))
+}
+
+func (c *Compiler) fieldIndex(name string) int {
+	i, ok := c.fields[name]
+	if !ok {
+		i = len(c.fields)
+		c.fields[name] = i
+	}
+	return i
+}
---
+++ k3/t3xf/compiler_test.go
@@ -0,0 +1,328 @@
+package t3xf_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/nokia/ntt/k3/t3xf"
+	"github.com/nokia/ntt/k3/t3xf/opcode"
+	"github.com/nokia/ntt/ttcn3/syntax"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompiler(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []string
+	}{
+		{input: "1", want: []string{
+			"natlong 1",
+		}},
+
+		{input: "1+2*3", want: []string{
+			"natlong 1",
+			"natlong 2",
+			"natlong 3",
+			"mul",
+			"add",
+		}},
+
+		{input: "var integer a := 1", want: []string{
+			"integer",
+			"name a",
+			"var",
+			"natlong 1",
+			"ref 40",
+			"assign",
+		}},
+
+		{input: "var integer a[1][2]", want: []string{
+			"integer",
+			"scan",
+			"natlong 2",
+			"block",
+			"array",
+			"scan",
+			"natlong 1",
+			"block",
+			"array",
+			"name a",
+			"var",
+		}},
+
+		{input: "const integer a := 1, b := 2", want: []string{
+			"scan",
+			"natlong 1",
+			"block",
+			"integer",
+			"name a",
+			"const",
+			"scan",
+			"natlong 2",
+			"block",
+			"integer",
+			"name b",
+			"const",
+		}},
+
+		{input: `module M {} with { extension "e" }`, want: []string{
+			"scan",
+			"utf8 e",
+			"extension",
+			"block",
+			"scan",
+			"block",
+			"name M",
+			"modulew",
+		}},
+
+		{input: `function f() {}`, want: []string{
+			"skip",
+			"skip",
+			"name f",
+			"function",
+		}},
+
+		{input: `function f(integer x, inout integer y, out template integer z) {}`, want: []string{
+			"scan",
+			"integer",
+			"name x",
+			"in",
+			"integer",
+			"name y",
+			"inout",
+			"integer",
+			"permitt",
+			"name z",
+			"out",
+			"block",
+			"skip",
+			"name f",
+			"function",
+		}},
+
+		{input: `function f() return integer {}`, want: []string{
+			"integer",
+			"skip",
+			"skip",
+			"name f",
+			"functionv",
+		}},
+
+		{input: `function f() return template integer {}`, want: []string{
+			"integer",
+			"permitt",
+			"skip",
+			"skip",
+			"name f",
+			"functionv",
+		}},
+
+		{input: `control {}`, want: []string{
+			"skip",
+			"control",
+		}},
+
+		{input: `type charstring T ("a".."z")`, want: []string{
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"subtype",
+			"name T",
+			"type",
+		}},
+
+		{input: `type charstring T length(2)`, want: []string{
+			"charstring",
+			"any",
+			"natlong 2",
+			"length",
+			"subtype",
+			"name T",
+			"type",
+		}},
+
+		{input: `type charstring T ("a".."z") length(2)`, want: []string{
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record of charstring T ("a".."z") length(2)`, want: []string{
+			//"scan",
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			//"block",
+			"recordof",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record length(1) of charstring T ("a".."z") length(2)`, want: []string{
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			"recordof",
+			"any",
+			"natlong 1",
+			"length",
+			"subtype",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { charstring F ("a".."z") }`, want: []string{
+			"scan",
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"subtype",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { charstring F length(2) }`, want: []string{
+			"scan",
+			"charstring",
+			"any",
+			"natlong 2",
+			"length",
+			"subtype",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { charstring F ("a".."z") length(2) }`, want: []string{
+			"scan",
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { record of charstring F ("a".."z") length(2) }`, want: []string{
+			"scan",
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			"recordof",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { record length(1) of charstring F ("a".."z") length(2) }`, want: []string{
+			"scan",
+			"charstring",
+			"utf8 a",
+			"utf8 z",
+			"range",
+			"natlong 2",
+			"length",
+			"subtype",
+			"recordof",
+			"any",
+			"natlong 1",
+			"length",
+			"subtype",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+
+		{input: `type record T { record {} ({},{}) length(2) }`, want: []string{
+			"scan",
+			"skip",
+			"record",
+			"mark",
+			"mark",
+			"vlist",
+			"mark",
+			"vlist",
+			"collect",
+			"natlong 2",
+			"length",
+			"subtype",
+			"name F",
+			"field",
+			"block",
+			"record",
+			"name T",
+			"type",
+		}},
+	}
+
+	for _, tt := range tests {
+		root, _, _ := syntax.Parse([]byte(tt.input))
+		if root.Err() != nil {
+			t.Fatalf("syntax.Parse(%q) returned error: %v", tt.input, root.Err())
+		}
+
+		c := t3xf.NewCompiler()
+		c.Compile(root)
+		b, err := c.Assemble()
+		if err != nil {
+			t.Fatalf("c.Assemble() returned error: %v", err)
+		}
+		got := []string{}
+		pc := 0
+		for pc < len(b) {
+			n, op, arg := t3xf.Decode(b[pc:])
+			pc += n
+			if op == opcode.LINE {
+				continue
+			}
+			s := op.String()
+			if arg != nil {
+				s += " " + fmt.Sprintf("%v", arg)
+			}
+			got = append(got, s)
+		}
+		assert.Equal(t, tt.want, got, tt.input)
+	}
+}
