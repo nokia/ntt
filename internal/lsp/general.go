7a79998d1bf91323b67b140433cda8c7f5aa83e9
Matthias Simon <matthias.simon@nokia.com>
2024-11-11 11:21:47 +0100

    Add proper version

--- internal/lsp/general.go
+++ internal/lsp/general.go
@@ -72,6 +72,11 @@ func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitializ
 
 	setTrace(params.Trace)
 
+	version := "devel"
+	if v, ok := ctx.Value("version").(string); ok {
+		version = v
+	}
+
 	return &protocol.InitializeResult{
 		Capabilities: protocol.ServerCapabilities{
 			CodeActionProvider:              false,
@@ -106,7 +111,7 @@ func (s *Server) initialize(ctx context.Context, params *protocol.ParamInitializ
 		ServerInfo: struct {
 			Name    string `json:"name"`
 			Version string `json:"version,omitempty"`
-		}{Name: "ntt", Version: "0.12.0"},
+		}{Name: "ntt", Version: version},
 	}, nil
 }
 
